edd0491a102fa059474e3ec294ebb93764085411
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-05-25 17:28:32 -0400

    chore: extend default theme and add global styles

---
+++ src/layouts/Home/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function HomePage () {
+  return (
+    <S.Container>
+      <h1>Home</h1>
+    </S.Container>
+  )
+}
---
+++ src/styles/theme/fonts.ts
@@ -0,0 +1,11 @@
+export const fonts = {
+  montserrat: "'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+  sizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem'
+  }
+}
