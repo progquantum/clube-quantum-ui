a7fea478e0959c3f2a9a5d62717d08200c9490a3
jtiagosantos <tiagosan040@gmail.com>
2022-05-27 07:28:15 -0300

    feat: implement Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function Header () {
+  return (
+    <S.Container>
+      <img src='/images/quantum-logo.svg' alt='Quantum logo' />
+    </S.Container>
+  )
+}
