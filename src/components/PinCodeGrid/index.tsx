471f14da3527c6f7ff330c046bad10dd725580f7
jtiagosantos <tiagosan040@gmail.com>
2022-05-31 09:35:35 -0300

    feat: implement PinCodeGrid component

---
+++ src/components/PinCodeGrid/index.tsx
@@ -0,0 +1,72 @@
+import { ChangeEvent, useRef } from 'react'
+
+import { StyledPinInput } from './styles'
+import { PinCodeGridProps } from './types'
+
+export function PinCodeGrid ({
+  pinCode,
+  onPinChange,
+  pinLength
+}: PinCodeGridProps) {
+  const inputRef = useRef<HTMLInputElement[]>([])
+
+  function changeInputFocus (pinCodeIndex: number) {
+    const input = inputRef.current[pinCodeIndex]
+
+    if (input) {
+      input.focus()
+    }
+  }
+
+  function onChange (
+    { target }: ChangeEvent<HTMLInputElement>,
+    index: number
+  ) {
+    const { value } = target
+    const pinCodeNumber = Number(value.trim())
+
+    if (isNaN(pinCodeNumber) || !value.length) {
+      return
+    }
+
+    onPinChange(pinCodeNumber, index)
+
+    if (index < pinLength - 1) {
+      changeInputFocus(index + 1)
+    }
+  }
+
+  const onKeyDown = (
+    { nativeEvent }: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const keyboardKeyCode = nativeEvent.code
+
+    if (keyboardKeyCode !== 'Backspace') {
+      return
+    }
+
+    if (!pinCode[index]) {
+      changeInputFocus(index - 1)
+      return
+    }
+
+    onPinChange(undefined, index)
+  }
+
+  return (
+    <div>
+      {Array.from({ length: pinLength }, (_, index) => (
+        <StyledPinInput
+          onKeyDown={(event) => onKeyDown(event, index)}
+          key={index}
+          ref={(element) => {
+            inputRef.current[index] = element || undefined
+          }}
+          onChange={(event) => onChange(event, index)}
+          value={pinCode[index]}
+        />
+      ))}
+    </div>
+  )
+}
