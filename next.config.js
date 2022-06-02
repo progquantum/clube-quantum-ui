838dacd196312caf2a2914b80e167b755b0363b0
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-06-02 17:57:40 -0400

    feat: generate sitemap in build time

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ module.exports = {
   swcMinify: true,
   compiler: {
     styledComponents: true
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./src/scripts/sitemap-generator')
+    }
+
+    return config
   }
 }
