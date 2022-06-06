d1c7dc77cc03353436379ed114c4c60b018cbe0c
jtiagosantos <tiagosan040@gmail.com>
2022-06-06 18:18:48 -0300

    style: add new color in theme

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -5,6 +5,7 @@ export const colors = {
   royalblue: '#0E5AE4',
   lightsteelblue: '#B2C5FF',
   darkslategray: '#373435',
+  mediumaquamarine: '#3CD2A2',
   white: '#ffffff',
   gray: {
     50: '#E6E6E6',
