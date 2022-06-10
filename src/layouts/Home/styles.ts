b24ee4230b532b87229f1ac8403b8672ac3467b1
jtiagosantos <tiagosan040@gmail.com>
2022-06-09 22:16:25 -0400

    refactor: refactor home page

--- src/layouts/Home/styles.ts
+++ src/layouts/Home/styles.ts
@@ -1,7 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.main`
   background: linear-gradient(180deg, #001F80 0%, #0C61FF 100%);
+  padding: 0 1rem;
 
   animation: show 0.3s ease-in forwards;
 
@@ -18,317 +19,166 @@ export const Container = styled.main`
   }
 `
 
-export const CashBack = styled.div`
-  height: 726.18px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  gap: 170px;
-`
-
-export const CashBackWrap = styled.div`
-  height: 533.18px;
-  display: flex;
-  justify-content: center;
-  gap: 2.5rem;
-`
-
-export const CashBackContent = styled.div`
-  width: 456px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  gap: 2.5rem;
-
-  .heading {
-    font-weight: 900;
-    color: ${({ theme }) => theme.colors.light};
-
-    &.title {
-      line-height: 3rem;
-      font-size: ${({ theme }) => theme.fontSizes['2xl']};
-
-    }
-
-    &.description {
-      line-height: 3rem;
-      font-size: ${({ theme }) => theme.fontSizes.lg};
-    }
-  }
-`
-
-export const FirstSection = styled.section`
-  max-width: 1200px;
+const wrapper = css`
+  max-width: 1600px;
   width: 100%;
   margin: 0 auto;
 
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   justify-content: space-between;
-
-  /* @media (max-width: 1500px) {
-    margin: 10rem 8rem 0;
-  }
-
-  @media (max-width: 935px) {
-    flex-direction: column-reverse;
-  }
-
-  @media (max-width: 550px) {
-    margin: 5rem auto;
-  } */
-
-  > div {
-    margin-top: 2rem;
-    color: #ffffff;
-
-    h1, h6 {
-      font-weight: 900;
-      margin-bottom: 2.5rem;
-    }
-
-    h1 {
-      font-size: 2.5rem;
-    }
-
-    h6 {
-      font-size: 1.125rem;
-    }
-
-    button {
-      background-color: #ffffff;
-      border-radius: 2.5rem;
-      padding: 1rem 1.875rem;
-      color: #001F80;
-      font-weight: 900;
-      font-size: 1rem;
-    }
-  }
-
-  /* > div {
-    margin-top: 6rem;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-
-    @media (max-width: 1050px) {
-      margin-top: 1rem;
-    }
-
-    @media (max-width: 935px) {
-      margin-bottom: 2rem;
-      align-items: center;
-    }
-  } */
-
-  img {
-    max-width: 42.5rem;
-    width: 100%;
-    height: auto;
-
-  /*  @media (max-width: 1300px) {
-      width: 25rem;
-      height: 25rem;
-    }
-    @media (max-width: 420px) {
-      width: 20rem;
-      height: 20rem;
-    } */
-  }
 `
 
-export const SecondSection = styled.section`
-  max-width: 1660px;
-  width: 100%;
-  margin: 3rem auto 0;
+export const CashBackWrapper = styled.section`
+  ${wrapper}
 
-  display: flex;
-  align-items: center;
-  gap: 4.375rem;
+  @media(max-width: 730px) {
+    padding-top: 3rem;
 
-  /* @media (max-width: 935px) {
-    flex-direction: column;
-  } */
-
-  img {
-    max-width: 29.4375rem;
-    width: 100%;
-    height: 32rem;
-
-    border-radius: 0.9375rem;
-    box-shadow: 20px 20px 40px #001f80;
-
-    /* @media (max-width: 1250px) {
-      width: 30rem;
-      height: 24rem;
-    }
-    @media (max-width: 430px) {
-      width: 20rem;
-      height: 16rem;
-    } */
-  }
-
-  > div {
-    max-width: 28.125rem;
-    width: 100%;
-
-    display: flex;
-    flex-direction: column;
-    gap: 4.375rem;
-    /* @media (max-width: 1250px) {
-      width: 50%;
-      padding-right: 1rem;
-    } */
-
-    div {
-      color: #ffffff;
-
-      h1 {
-        font-weight: 900;
-        font-size: 2.5rem;
-        margin-bottom: 1.875rem;
-      }
-
-      p {
-        font-weight: 500;
-        font-size: 1.25rem;
-      }
+    > span {
+      display: none !important;
     }
   }
 `
 
-export const ThirdSection = styled.section`
-  max-width: 1660px;
+export const CashBackContent = styled.div`
+  max-width: 734px;
   width: 100%;
 
   display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  gap: 2rem;
+  flex-direction: column;
+  align-items: flex-start;
 
-  position: relative;
-  z-index: 1;
-
-  /* @media (max-width: 935px) {
-    flex-direction: column-reverse;
-  } */
-
-  img {
-    max-width: 30rem;
-    width: 100%;
-    height: 35rem;
-
-    border-radius: 0.9375rem;
-    box-shadow: 10px 40px 30px #001f80;
-
-    /* margin-bottom: -1.75rem;
-    z-index: 1; */
-
-    /* @media (max-width: 1250px) {
-      width: 37rem;
-      height: 24rem;
-    }
-    @media (max-width: 530px) {
-      width: 27rem;
-      height: 17rem;
-    }
-    @media (max-width: 380px) {
-      width: 20rem;
-      height: 13rem;
-    } */
-  }
-
-  > div {
-    max-width: 24.6875rem;
-    width: 100%;
-
-    color: #ffffff;
-
-    h1 {
-      font-weight: 900;
-      font-size: 2.5rem;
-      margin-bottom: 1.875rem;
-    }
-
-    p {
-      font-weight: 500;
-      font-size: 1.25rem;
-    }
-
-    /* @media (max-width: 740px) {
-      margin-left: 0;
-      align-items: center;
-    } */
-  }
-`
-
-export const FaqContents = styled.section`
-  max-width: 1660px;
-  width: 100%;
-  color: #ffffff;
-  background: linear-gradient(180deg, #001F80 0%, #0C61FF 100%);
-  padding: 10rem 0 0;
-
-  transform: translateY(-100px);
-  position: relative;
-  z-index: 0;
+  color: ${({ theme }) => theme.colors.white};
 
   h1 {
-    max-width: 1220px;
-    width: 100%;
-    margin: 0 auto;
-
     font-size: 2.5rem;
     font-weight: 900;
-    margin-bottom: 3.125rem;
+    margin-bottom: 2.5rem;
   }
 
-  > div {
+  h6 {
+    font-size: 1.75rem;
+    font-weight: 500;
+    margin-bottom: 2.5rem;
+  }
+
+  button {
+    max-width: 250px;
     width: 100%;
-    margin: 0 auto;
+    color: ${({ theme }) => theme.colors.midnightBlue};
+    font-weight: 900;
+    font-size: 1rem;
+  }
 
-    display: flex;
-    justify-content: space-between;
-    max-width: 1220px;
+  @media(max-width: 730px) {
+    align-items: center;
+  }
+`
 
-    img {
-      max-width: 37.5rem;
-      width: 100%;
-      height: 26.25rem;
+export const ArrowDownWrapper = styled.div`
+  width: 100%;
+  margin: 5rem 0;
+
+  display: flex;
+  justify-content: center;
+`
+
+export const Wrapper = styled.section`
+  max-width: 1920px;
+  width: 100%;
+  margin: 0 auto;
+`
+
+export const Content = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  &.second-content {
+    flex-direction: row-reverse;
+
+    div {
+      width: 50%;
+      margin-left: 0;
+      margin-right: 50px;
     }
+  }
+`
 
-    > div {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 2.1875rem;
+export const TextContent = styled.div`
+  width: 50%;
+  margin-left: 100px;
 
-      button {
-        background-color: #ffffff;
-        border-radius: 2.5rem;
-        padding: 1rem 1.875rem;
-        color: #001F80;
-        font-weight: 900;
-        font-size: 1rem;
-      }
+  color: ${({ theme }) => theme.colors.white};
+
+  h1 {
+    font-weight: 900;
+    margin-bottom: 30px;
+    font-size: 2rem;
+
+    &:last-of-type {
+      margin-top: 70px;
     }
+  }
+
+  h6 {
+    font-weight: 500;
+    font-size: 1.25rem;
+  }
+`
+
+export const FaqWrapper = styled.section`
+  ${wrapper}
+  margin: 5rem auto 2rem;
+  padding-bottom: 7rem;
+
+  @media(max-width: 1000px) {
+    justify-content: center;
+
+    > span {
+      display: none !important;
+    }
+  }
+`
+
+export const FaqContent = styled.div`
+  max-width: 472px;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.white};
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  h1 {
+    font-weight: 900;
+    font-size: 1.75rem;
+    margin-bottom: 50px;
   }
 `
 
 export const Box = styled.div`
-  max-width: 31.875rem;
   width: 100%;
-  padding: 2rem;
+  max-height: 290px;
+  padding: 50px;
+  border-radius: 30px;
+  border: 3px solid ${({ theme }) => theme.colors.white};
 
-  border: 3px solid #ffffff;
-  border-radius: 1.25rem;
+  &:last-of-type {
+    margin-top: 30px;
+  }
 
-  h3 {
-    font-size: 1.75rem;
+  h2 {
+    font-size: 1.5rem;
     font-weight: 700;
-    margin-bottom: 1.5625rem;
+    margin-bottom: 25px;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5rem;
   }
 `
