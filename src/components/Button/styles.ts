4974a63c5a23611451f6454ec0b241550ecc49fb
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-06-13 15:20:07 -0400

    feat: add align center in button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
-  width: 15.6rem;
-  height: 2.75rem;
-  background: ${({ theme }) => theme.colors.light};
-  color: ${({ theme }) => theme.colors.midnightBlue};
-  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
