dac0626b3ecd197b2075ae9538c7e02009327d54
Victor Kempfer <victor.kempfer77@gmail.com>
2022-06-16 17:28:38 -0400

    refactor: delete import button

--- src/layouts/ForgotPassword/index.tsx
+++ src/layouts/ForgotPassword/index.tsx
@@ -2,9 +2,8 @@ import Image from 'next/image'
 
 import { useForm } from 'react-hook-form'
 
-import { Input } from 'components/Input'
 import { Header } from 'components/Header'
-import { Button } from 'components/Button'
+import { Input } from 'components/Input'
 import { Footer } from 'components/Footer'
 
 import * as S from './styles'
