adba0b1737cae26b8eda6a89d8d193be0378a589
Itsrasfa <rafaelacontatoss@gmail.com>
2022-07-27 14:53:25 -0300

    refactor: container size

--- src/layouts/InviteFriendsError/styles.ts
+++ src/layouts/InviteFriendsError/styles.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 
 export const Container = styled.main`
   width: 100%;
-  max-width: 70.725rem;
+  max-width: 75rem;
   margin: 0 auto;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 3.5rem;
+  padding: 5rem 2rem;
 
   h2 {
     font-weight: 900;
@@ -27,16 +28,11 @@ export const Container = styled.main`
 `
 
 export const Content = styled.div`
-  margin: 0 0 0 2rem;
-  @media (min-width: 280px) and (max-width: 767px) {
-   margin: 2rem;
-  }
+
 `
 export const Image = styled.div`
-  margin: 0 2rem 0 0;
   @media (min-width: 280px) and (max-width: 767px) {
    max-width: 230px;
    order: -1;
-   margin: 2rem;
   }
 `
