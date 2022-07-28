e018a5541cf3604efde03e25b2f0b849a840ea66
Itsrasfa <rafaelacontatoss@gmail.com>
2022-07-28 14:27:58 -0300

    refactor: change content to invite friends

--- src/layouts/InviteFriends/index.tsx
+++ src/layouts/InviteFriends/index.tsx
@@ -6,12 +6,16 @@ import { Header } from 'components/Header'
 import { useFindMe } from 'hooks/useFindMe'
 import { useShare } from 'hooks/useShare'
 
+import { Button } from 'components/Button'
+
 import * as S from './styles'
 
 export function InviteFriendsPage () {
-  const { data } = useFindMe()
+  const { data, isLoading } = useFindMe()
 
   const inviteCode = `http://localhost:3000/signup/${data?.invite_code}`
+
+  const isInviteCode = data?.invite_code
 
   const share = useShare()
 
@@ -22,52 +26,74 @@ export function InviteFriendsPage () {
       url: inviteCode
     })
   }
-
+  if (isLoading) return <h1>Carregando...</h1>
   return (
     <>
       <title>Convidar Amigos</title>
 
       <Header />
+      {isInviteCode
+        ? (
+          <S.Container>
+            <S.Wrap>
+              <S.Heading>Nós damos valor às amizades</S.Heading>
+              <S.Content>
+                E recompensamos você, para cada Amigo convidado que se cadastrar no Clube Quantum.
+              </S.Content>
 
-      <S.Container>
-        <S.Wrap>
-          <S.Heading>Nós damos valor às amizades</S.Heading>
-          <S.Content>
-            E recompensamos você, para cada Amigo convidado que se cadastrar no Clube Quantum.
-          </S.Content>
+              <S.LinkButton variant='secondary' onClick={handleShare}>
+                Seu link de convite é: <strong>{inviteCode}</strong>
+              </S.LinkButton>
 
-          <S.LinkButton variant='secondary' onClick={handleShare}>
-            Seu link de convite é: <strong>{inviteCode}</strong>
-          </S.LinkButton>
+              <S.ContainerLinks>
+                <S.InviteButton>
+                  <S.Instagram />
+                  Convidar via Instagram
+                </S.InviteButton>
+                <S.InviteButton>
+                  <S.Facebook />
+                  Convidar via Facebook
+                </S.InviteButton>
+                <S.InviteButton>
+                  <S.WhatsApp />
+                  Convidar via WhatsApp
+                </S.InviteButton>
+                <S.InviteButton>
+                  <S.Mail />
+                  Convidar via E-Mail
+                </S.InviteButton>
+                <S.InviteButton onClick={handleShare}>
+                  <S.Link />
+                  Copiar o link do convite
+                </S.InviteButton>
+              </S.ContainerLinks>
+            </S.Wrap>
 
-          <S.ContainerLinks>
-            <S.InviteButton>
-              <Image width={34} height={33} src='/images/instagram-blue.svg' alt='Instagram' />
-              Convidar via Instagram
-            </S.InviteButton>
-            <S.InviteButton>
-              <Image width={34} height={33} src='/images/facebook-blue.svg' alt='Facebook' />
-              Convidar via Facebook
-            </S.InviteButton>
-            <S.InviteButton>
-              <Image width={34} height={33} src='/images/whatsapp-blue.svg' alt='WhatsApp' />
-              Convidar via WhatsApp
-            </S.InviteButton>
-            <S.InviteButton>
-              <Image width={34} height={33} src='/images/email-blue.svg' alt='E-Mail' />
-              Convidar via E-Mail
-            </S.InviteButton>
-            <S.InviteButton onClick={handleShare}>
-              <Image width={34} height={33} src='/images/link-blue.svg' alt='Link do convite' />
-              Copiar o link do convite
-            </S.InviteButton>
-          </S.ContainerLinks>
-        </S.Wrap>
+            <S.ImageDiv>
+              <Image className='image' width={385} height={467} src='/images/friends.svg' alt='' />
+            </S.ImageDiv>
+          </S.Container>
+          )
+        : (
+          <S.Container>
+            <S.Wrap>
+              <S.Heading>Oops, você ainda não pode convidar amigos!</S.Heading>
+              <S.Content>
+                Conheça os planos disponíveis no Clube Quantum, e clique abaixo para acessar e começar usufruir dos benefícios.
+              </S.Content>
 
-        <S.ImageDiv>
-          <Image className='image' width={385} height={467} src='/images/friends.svg' alt='' />
-        </S.ImageDiv>
-      </S.Container>
+              <S.ButtonPlan>
+                <S.HeadingInfo>
+                  Para adquirir um plano quantum, basta clicar no botão abaixo e inserir as informações necessárias.
+                </S.HeadingInfo>
+                <S.InfoButton>Comece agora</S.InfoButton>
+              </S.ButtonPlan>
+            </S.Wrap>
+            <S.ImageDiv>
+              <Image className='image' width={385} height={467} src='/images/friends-error.svg' alt='' />
+            </S.ImageDiv>
+          </S.Container>
+          )}
       <Footer />
     </>
   )
--- src/layouts/InviteFriends/styles.ts
+++ src/layouts/InviteFriends/styles.ts
@@ -1,23 +1,28 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
+import { HiOutlineMail, HiOutlineLink } from 'react-icons/hi'
+import { FiFacebook } from 'react-icons/fi'
 
 import { Button } from 'components/Button'
 
 export const Container = styled.main`
   width: 100%;
-  max-width: 70.625rem;
+  max-width: 75rem;
   margin: 0 auto;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  margin-block: 5rem;
+  justify-content: center;
+  gap: 4.5rem;
+  padding: 5rem 2rem;
 `
 
 export const Heading = styled.h1`
   font-weight: 900;
   font-size: 2rem;
   line-height: 1.4;
-  margin: 5rem 0 1.875rem;
   max-width: 18ch;
+  margin-bottom: 2rem;
 `
 
 export const Content = styled.p`
@@ -25,29 +30,42 @@ export const Content = styled.p`
   font-size: 1.25rem;
   line-height: 1.3;
   max-width: 30ch;
-  margin-bottom: 2.5rem;
 `
 
 export const LinkButton = styled(Button)`
   align-items: start;
   flex-direction: column;
   padding: 1rem 1.875rem;
-  margin-bottom: 2.25rem;
-  
+  margin-block: 2.2rem;
+  line-height: 1.5;
   border: 2px solid ${({ theme }) => theme.colors.midnightBlue};
   border-radius: .625rem;
 `
+const iconStyles = css`
+  background-color: ${({ theme }) => theme.colors.midnightBlue};
+  color: ${({ theme }) => theme.colors.white};
+  width: 1.875rem;
+  height: 1.875rem;
+  padding: .3rem;
+  border-radius: 50%;
+`
+export const Instagram = styled(FaInstagram)`
+  ${iconStyles};
+`
+export const Facebook = styled(FiFacebook)`
+  ${iconStyles};
+`
+export const WhatsApp = styled(FaWhatsapp)`
+  ${iconStyles};
+`
 
+export const Mail = styled(HiOutlineMail)`
+  ${iconStyles};
+`
+export const Link = styled(HiOutlineLink)`
+  ${iconStyles};
+`
 export const Wrap = styled.div`
-  @media (min-width: 280px) and (max-width: 767px) {
-    margin-inline: 2.5rem;
-  }
-
-
-  @media (min-width: 768px) and (max-width: 992px) {
-    margin-inline: 2.5rem;
-    margin-bottom: 1rem;
-  }
 `
 
 export const ImageDiv = styled.div`
@@ -86,4 +104,19 @@ export const InviteButton = styled(Button)`
   line-height: 22px;
   color: ${({ theme }) => theme.colors.gray[700]};
   background-color: transparent;
+`
+export const ButtonPlan = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const HeadingInfo = styled.h2`
+  margin: 2.5rem 0;
+  max-width: 25ch;
+  line-height: 1.3;
+  font-weight: 900;
+`
+export const InfoButton = styled(Button)`
+  font-weight: 600;
+  padding: .7rem 0;
 `
