c8e8401e1e7365e7fed71cf6736c1b01a1964a26
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-08-01 17:02:06 -0400

    feat: recovery invite code in signup

--- src/constants/routesPath.ts
+++ src/constants/routesPath.ts
@@ -5,7 +5,7 @@ export const SIGN_IN_PAGE = '/signin'
 export const HOMEPAGE_PAGE = '/'
 export const ADVANTAGES_PAGE = '/advantages'
 
-export const SIGN_UP_PAGE = '/signup/personal'
+export const SIGN_UP_PAGE = '/signup/'
 export const SIGN_UP_BUSINESS_PAGE = '/signup/business'
 export const SIGN_UP_PERSONAL_PAGE = '/signup/personal'
 
