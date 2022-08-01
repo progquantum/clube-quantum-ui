dbc34eca36d31dccbd3f5af19ce0f805d80159bb
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-08-01 18:16:55 -0300

    feat: create component Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,107 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Button } from 'components/Button'
+
+import * as S from './styles'
+
+export function Menu () {
+  return (
+    <S.Container>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={19.15} height={24} src='/images/icon-my-account.svg' alt='Icone Minha Conta' />
+            </S.DivImage>
+            <S.Text>Minha Conta</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={24} height={24} src='/images/icon-update-user.svg' alt='Icone Atualizar Cadastro' />
+            </S.DivImage>
+            <S.Text>Atualizar Cadastro</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={13.5} height={24} src='/images/icon-bank-statement.svg' alt='Icone Extratos' />
+            </S.DivImage>
+            <S.Text>Extratos</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={24} height={24} src='/images/icon-my-friends.svg' alt='Icone Meus Amigos' />
+            </S.DivImage>
+            <S.Text>Meus Amigos</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={21.18} height={24} src='/images/icon-my-orders.svg' alt='Icone Meus Pedidos' />
+            </S.DivImage>
+            <S.Text>Meus Pedidos</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={21.6} height={24} src='/images/icon-plans.svg' alt='Icone Planos' />
+            </S.DivImage>
+            <S.Text>Planos</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={21.6} height={24} src='/images/icon-licenses.svg' alt='Icone Lincenças' />
+            </S.DivImage>
+            <S.Text>Lincenças</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Link href='/'>
+        <S.ItemContainer>
+          <S.Content>
+            <S.DivImage>
+              <Image width={18.46} height={24} src='/images/icon-privacy-center.svg' alt='Icone Central de Privacidade' />
+            </S.DivImage>
+            <S.Text>Central de Privacidade</S.Text>
+          </S.Content>
+        </S.ItemContainer>
+      </Link>
+
+      <Button variant='danger'>
+        <S.Text>Sair</S.Text>
+        <Image width={16} height={16} src='/images/icon-logout.svg' alt='Icone Sair' />
+      </Button>
+
+    </S.Container>
+  )
+}
---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const Container = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 18.563rem;
+  height: 46.125rem;
+  background-color: ${({ theme }) => theme.colors.background};
+  box-shadow: 0 0 1.875rem rgba(0, 0, 0, 0.08);
+  border-radius: 0.625rem;
+  @media (max-width: 414px){
+    display: none;
+  }
+`
+
+export const ItemContainer = styled.div`
+  cursor: pointer;
+  display: flex;
+  height: 5rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[100]};
+  width: 15.625rem;
+
+`
+
+export const Content = styled.div`
+  display: flex;
+  gap: 1.151rem;
+  margin: 1.875rem 0;
+  margin-left: 0.964rem;
+`
+
+export const DivImage = styled.div`
+  width: 1.563rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`
+
+export const Text = styled.p`
+  font-weight: 500;
+  font-size: 1rem;
+
+`
