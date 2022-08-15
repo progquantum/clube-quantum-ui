478f3ddee30ad1d07d2f8bb05567db67c83fecec
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-15 15:46:26 -0300

    refactor: add skeleton for credit card and bank account components

---
+++ src/layouts/ManagePayment/BankAccount/Skeleton/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import Skeleton from 'react-loading-skeleton'
+
+export const BankAccountSkeleton = styled(Skeleton)`
+  width: 21.844rem;
+  height: 13.625rem;
+  border-radius: .9rem;
+
+  @media (max-width: 1024px){
+    width: 26.688rem;
+  }
+
+  @media (max-width: 460px){
+    width: 20.665rem;
+  }
+`
