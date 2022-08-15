c2b9d7aac6cb8b898b97e9e384af1c6fa9c7ad63
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-15 15:53:31 -0300

    feat: add new types to FindBilling

--- src/shared/types/apiSchema.ts
+++ src/shared/types/apiSchema.ts
@@ -22,3 +22,19 @@ export type Address = {
   state: string,
   country: string
 }
+
+export type FindBilling = {
+  credit_card?: {
+    last_digits: string,
+    brand: string,
+    expiration_date: string
+  },
+  bank_account?: {
+    bank_code: string,
+    bank_name: string,
+    agency: string,
+    holder_name: string,
+    current_account: string,
+    current_account_check_number: string
+  }
+}
