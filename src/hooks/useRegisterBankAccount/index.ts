76498b57ecef99a59f13c951d087fd558ee1a39d
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-17 16:46:20 -0300

    refactor(hooks): add promise return to register bank account

--- src/hooks/useRegisterBankAccount/index.ts
+++ src/hooks/useRegisterBankAccount/index.ts
@@ -3,8 +3,11 @@ import { useMutation } from 'react-query'
 import { api } from 'config/client'
 
 import { RegisterBankAccountData } from './types'
-export const registerBankAccount = (data: RegisterBankAccountData) => (
-  api.post('/bank-accounts', data)
+
+export const registerBankAccount = (
+  data: RegisterBankAccountData
+): Promise<unknown> => (
+  api.post<unknown>('/bank-accounts', data)
 )
 
 export function useRegisterBankAccount () {
