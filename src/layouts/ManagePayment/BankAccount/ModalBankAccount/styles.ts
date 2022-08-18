8ca0f151dfcd4c81ee9fc44531f84cac5fc50753
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-18 17:50:08 -0300

    refactor: remove unused styles and formatting code of the bank account component modal

--- src/layouts/ManagePayment/BankAccount/ModalBankAccount/styles.ts
+++ src/layouts/ManagePayment/BankAccount/ModalBankAccount/styles.ts
@@ -2,33 +2,6 @@ import styled from 'styled-components'
 
 import { Button } from 'components/Button'
 
-export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.8);
-    z-index: 10;
-    position: fixed;
-    top: 0;
-    right: 0;
-    -webkit-overflow-scrolling: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
-export const ModalBox = styled.div`
-    width: 21rem;
-    background-color: ${({ theme }) => theme.colors.white};
-    border-radius: .9rem;
-    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-    padding: 1.25rem 1.5rem;
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 12%;
-    margin: 0 auto;
-    z-index: 1000;
-`
 export const ContentTitle = styled.h2`
     font-size: 0.75rem;
     color: ${({ theme }) => theme.colors.gray[400]};
@@ -63,6 +36,7 @@ export const TextContent = styled.p`
     color: ${({ theme }) => theme.colors.gray[700]};
     font-weight: 900;
 `
+
 export const BankingAccountForm = styled.form`
     display:flex;
     flex-direction: column;
@@ -104,12 +78,14 @@ export const BankingConfirmAccount = styled.div`
     display: flex;
     justify-content: space-between;
 `
+
 export const BankingConfirmData = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
     margin-bottom: 1.5rem;
 `
+
 export const TextConfirmAccount = styled.p`
     color: ${({ theme }) => theme.colors.gray[700]};
     font-weight: 500;
