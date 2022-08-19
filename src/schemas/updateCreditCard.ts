de33e0fd4b196ca0800814a869f9ea46afa3b615
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-19 17:39:35 -0300

    feat: add yup schemas for update credit card

---
+++ src/schemas/updateCreditCard.ts
@@ -0,0 +1,9 @@
+import Yup from 'config/yup'
+
+export const schema = Yup.object().shape({
+  card_number: Yup.string().required().matches(/^(4\d{3})\s(\d{4})\s(\d{4})\s(\d{4})$/, 'Cartão de crédito inválido!'),
+  card_name: Yup.string().required(),
+  expiration_date: Yup.string().required(),
+  cvc: Yup.string().required(),
+  card_brand: Yup.string().required()
+})
