e162ba903d04eb1bceb60208f0bb6c9081780254
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-08-19 17:36:00 -0300

    feat: adjust yup mesage

--- src/schemas/updatePlan.ts
+++ src/schemas/updatePlan.ts
@@ -3,9 +3,9 @@ import Yup from 'config/yup'
 export const cvcSchema = Yup.object().shape({
   cvc: Yup
     .string()
-    .min(3)
     .max(3)
+    .min(3)
     .required()
-    .matches(/[0-9]/g)
+    .matches(/[0-9]/g, 'Campo deve conter somente números')
 
 })
