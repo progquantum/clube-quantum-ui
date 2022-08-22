52558ba21e1651d556bd7cfaa29285bb1e9ee217
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-08-22 16:38:37 -0300

    refactor: add new props, price and plan_name to context

--- src/contexts/plans/types.ts
+++ src/contexts/plans/types.ts
@@ -1,8 +1,10 @@
 import { Dispatch, SetStateAction } from 'react'
 
 export type Plans = {
-    plan_id: string
-    plan_duration: number
+    plan_id?: string
+    plan_duration?: number
+    price?: string
+    plan_name?: string
 }
 
 export type PlansStateContextData = {
