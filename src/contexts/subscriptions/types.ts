7751496aabd502676ea6f5a13f3b61afa92ce4c6
anajuliarauber <anajuliarauber@hotmail.com>
2022-08-23 17:58:42 -0300

    feat: rename plans to subscriptions, add bank account account and credit card props

---
+++ src/contexts/subscriptions/types.ts
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction } from 'react'
+
+export type Plans = {
+    plan_id?: string
+    plan_duration?: number
+    price?: string
+    plan_name?: string
+}
+
+export type BankAccount = {
+  current_account: string,
+  current_account_check_number: string,
+  holder_name: string
+}
+
+export type CreditCard = {
+  card_name: string,
+  card_number: string,
+  expiration_date: string,
+  cvc: string
+}
+
+export type SubscriptionsStateContextData = {
+  plan: Plans,
+  bankAccount: BankAccount,
+  creditCard: CreditCard
+}
+
+export type SubscriptionsDispatchContextData = {
+  setPlan: Dispatch<SetStateAction<Plans>>,
+  setBankAccount: Dispatch<SetStateAction<BankAccount>>,
+  setCreditCard: Dispatch<SetStateAction<CreditCard>>,
+}
