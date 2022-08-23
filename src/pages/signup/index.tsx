a4f3ac26b71207be2e7dda8f222f88385b484ec5
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-23 17:15:16 -0300

    refactor: add functionality to check if the invite code is valid

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,8 +2,29 @@ import { GetServerSideProps } from 'next'
 
 import { SignUpPage } from 'layouts/SignUp'
 import { withSSRGuest } from 'helpers/auth/withSSRGuest'
+import { checkInviteCode } from 'services/resources'
+import { INVITE_CODE_NOT_FOUND } from 'constants/routesPath'
 
-export const getServerSideProps: GetServerSideProps = withSSRGuest(async () => {
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
+  const { invite } = ctx.query
+
+  if (!invite) {
+    return {
+      props: {}
+    }
+  }
+
+  const { is_valid } = await checkInviteCode(invite)
+
+  if (!is_valid) {
+    return {
+      redirect: {
+        destination: INVITE_CODE_NOT_FOUND,
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {}
   }
