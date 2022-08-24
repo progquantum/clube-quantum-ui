492de0ba2dffc4e7b3ef535c33f30aa975a623d4
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-24 11:44:50 -0300

    refactor: add regex to validate banco um card

--- src/schemas/updateCreditCard.ts
+++ src/schemas/updateCreditCard.ts
@@ -1,7 +1,7 @@
 import Yup from 'config/yup'
 
 export const schema = Yup.object().shape({
-  card_number: Yup.string().required().matches(/^(4\d{3})\s(\d{4})\s(\d{4})\s(\d{4})$/, 'Cartão de crédito inválido!'),
+  card_number: Yup.string().required().matches(/^(4654)\s(47\d{2})\s(\d{4})\s(\d{4})$/, 'Cartão de crédito inválido!'),
   card_name: Yup.string().required(),
   expiration_date: Yup.string().required(),
   cvc: Yup.string().required(),
