4471df831ac9787ab8664f1b4d5a7d3cd95a3854
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-08-25 16:42:07 -0300

    refactor: move FormCreditCard to index CreditCard and add yup

--- src/layouts/Subscriptions/CreditCard/index.tsx
+++ src/layouts/Subscriptions/CreditCard/index.tsx
@@ -1,18 +1,139 @@
-import { Footer } from 'components/Footer'
-import { Header } from 'components/Header'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import { useTheme } from 'styled-components'
+
 import { SideBar } from 'components/SideBar'
+import { Header } from 'components/Header'
+import { Footer } from 'components/Footer'
+import { VISAIcon } from 'components/Illustrations/Visa'
+import { CreditCardIcon } from 'components/Illustrations/CreditCard'
 
-import { FormCreditCard } from './FormCreditCard'
+import { creditCardSchema } from 'schemas/createSubscription'
+import { BANK_ACCOUNT_PAGE, FINISHED_SUBSCRIPTION, SUBSCRIPTIONS_PAGE } from 'constants/routesPath'
+import { useSubscriptionsDispatch } from 'contexts/subscriptions/SubscriptionsContext'
+import { formatCreditCard } from 'utils/formatters/formatCreditCard'
+import { formatCreditCardExpiration } from 'utils/formatters/formatCreditCardExpiration'
 
+import { FormCreditCardData } from './types'
 import * as S from './styles'
 
 export function CreditCardPage () {
+  const {
+    control,
+    handleSubmit,
+    formState,
+    register,
+    setValue
+  } = useForm<FormCreditCardData>({
+    defaultValues: {
+      card_name: '',
+      card_number: '',
+      expiration_date: '',
+      cvc: ''
+    },
+    resolver: yupResolver(creditCardSchema)
+  })
+  const { isDirty, isSubmitting } = formState
+  const isButtonDisabled = !isDirty || isSubmitting
+  const router = useRouter()
+  const { colors } = useTheme()
+  const { registerCreditCard } = useSubscriptionsDispatch()
+
+  const onSubmit: SubmitHandler<FormCreditCardData> = (data) => {
+    registerCreditCard(data)
+    router.push(FINISHED_SUBSCRIPTION)
+  }
+
   return (
     <>
       <Header />
       <S.Main>
         <SideBar />
-        <FormCreditCard />
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <S.Line>
+            <CreditCardIcon
+              color={colors.gray[100]}
+              width='22'
+              height='16'
+            />
+            Seu cartão cadastrado
+          </S.Line>
+          <S.Text>
+            Para poder tirar o máximo de proveito dos benefícios da plataforma,
+            você precisa adicionar um cartão
+          </S.Text>
+          <S.DivInput>
+            <S.Label>Nome do Cartão</S.Label>
+            <S.Input
+              control={control}
+              name='card_name'
+              type='text'
+              placeholder='Nome impresso no cartão'
+            />
+          </S.DivInput>
+          <S.ContentCardNumber>
+            <S.DivInput>
+              <S.Label>Número do Cartão</S.Label>
+              <S.Input
+                control={control}
+                name='card_number'
+                type='text'
+                placeholder='0000 0000 0000 0000'
+                {...register('card_number', {
+                  onChange: (e) => {
+                    setValue('card_number', formatCreditCard(e.target.value))
+                  }
+                })}
+              />
+            </S.DivInput>
+            <VISAIcon width='50' height='17.18' />
+          </S.ContentCardNumber>
+          <S.ContentCardExpirateCVC>
+            <S.DivInput>
+              <S.Label>Data de Vencimento</S.Label>
+              <S.Input
+                control={control}
+                name='expiration_date'
+                type='text'
+                placeholder='00/0000'
+                {...register('expiration_date', {
+                  onChange: (e) => {
+                    setValue('expiration_date', formatCreditCardExpiration(e.target.value))
+                  }
+                })}
+              />
+            </S.DivInput>
+            <S.CVC>
+              <S.Label>CVC</S.Label>
+              <S.Input
+                control={control}
+                name='cvc'
+                type='text'
+                placeholder='000'
+              />
+            </S.CVC>
+          </S.ContentCardExpirateCVC>
+          <S.ConfirmButton
+            variant='secondary_degrade'
+            type='submit'
+            disabled={isButtonDisabled}
+          >
+            Confirmar
+          </S.ConfirmButton>
+          <Link href={BANK_ACCOUNT_PAGE}>
+            <S.ReturnButton
+              variant='secondary'
+            >
+              Voltar
+            </S.ReturnButton>
+          </Link>
+          <Link href={SUBSCRIPTIONS_PAGE}>
+            <S.Cancel>Cancelar</S.Cancel>
+          </Link>
+        </S.Form>
       </S.Main>
       <Footer />
     </>
