7d8bce946b60c8db8a7342d094f1bb34a1b0146b
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-08-25 16:48:35 -0300

    feat: add new formatBankAccount

---
+++ src/utils/formatters/formatBankAccount.ts
@@ -0,0 +1,9 @@
+export function formatBankAccount (account: string) {
+  let formattedaccount = account.replace(/[^\d]/g, '')
+  const formataccountLenght = formattedaccount.length
+
+  if (formataccountLenght > 3) {
+    formattedaccount = formattedaccount.slice(0, formataccountLenght - 1) + '-' + formattedaccount.slice(-1)
+  }
+  return formattedaccount
+}
