71b1c2dad8192fc5231ed78d09b432660d1eb996
itsrasfa <rafaelacontatoss@gmail.com>
2022-08-30 15:02:34 -0300

    refactor: add regex to validate card expiration date and cvv

--- src/schemas/updateCreditCard.ts
+++ src/schemas/updateCreditCard.ts
@@ -3,7 +3,7 @@ import Yup from 'config/yup'
 export const schema = Yup.object().shape({
   card_number: Yup.string().required().matches(/^(4654)\s(47\d{2})\s(\d{4})\s(\d{4})$/, 'Cartão de crédito inválido!'),
   card_name: Yup.string().required(),
-  expiration_date: Yup.string().required(),
-  cvc: Yup.string().required(),
+  expiration_date: Yup.string().required().matches(/(0[1-9]|10|11|12)[/](20\d{2})/, 'Validade do cartão inválida'),
+  cvc: Yup.string().required().matches(/\d{3}/, 'Informe um CVV com 3 dígitos'),
   card_brand: Yup.string().required()
 })
