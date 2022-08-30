f69b845386d6f08baf13b6d42ac145cc9f61e610
anajuliarauber <anajuliarauber@hotmail.com>
2022-08-30 17:42:32 -0300

    feat: implement toast for when user already owns a bank account

--- src/layouts/Subscriptions/Finished/index.tsx
+++ src/layouts/Subscriptions/Finished/index.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { MdAssignmentInd } from 'react-icons/md'
 import Link from 'next/link'
-
 import { useTheme } from 'styled-components'
+import { AxiosError } from 'axios'
 
 import { Footer } from 'components/Footer'
 import { Header } from 'components/Header'
@@ -12,20 +12,19 @@ import { Error } from 'components/Error'
 import { Successful } from 'components/Successful'
 import { BancoUm } from 'components/Illustrations/BancoUm'
 import { CreditCardIcon } from 'components/Illustrations/CreditCard'
-
 import { formatFirstLetterToUppercase } from 'utils/formatters/formatFirstLetterToUppercase'
 import { formatPrice } from 'utils/formatters/formatPrice'
 import { CREDIT_CARD_PAGE } from 'constants/routesPath'
 import { useSubscriptionsState } from 'contexts/subscriptions/SubscriptionsContext'
 import { useRegisterSubscription } from 'hooks/useRegisterSubscription'
+import { error } from 'helpers/notify/error'
+import { formatCreditCardRemoveSpace } from 'utils/formatters/formatCreditCard'
 
 import * as S from './styles'
+import { ErrorResponse } from './types'
 
 export function FinishedPage () {
-  const {
-    handleSubmit,
-    formState
-  } = useForm({
+  const { handleSubmit, formState } = useForm({
     defaultValues: {
       plan: {
         plan_id: '',
@@ -44,10 +43,8 @@ export function FinishedPage () {
       }
     }
   })
-  const {
-    mutate: creatSubscription,
-    isLoading: isCreating
-  } = useRegisterSubscription()
+  const { mutate: creatSubscription, isLoading: isCreating } =
+    useRegisterSubscription()
 
   const { isSubmitting } = formState
   const isButtonDisabled = isSubmitting || isCreating
@@ -57,27 +54,48 @@ export function FinishedPage () {
   const { plan, bankAccount, creditCard } = useSubscriptionsState()
 
   function onSubmit () {
-    creatSubscription({
-      plan: {
-        plan_id: plan.plan_id,
-        plan_duration: plan.plan_duration
+    creatSubscription(
+      {
+        plan: {
+          plan_id: plan.plan_id,
+          plan_duration: plan.plan_duration
+        },
+        bank_account: {
+          current_account: bankAccount.current_account,
+          current_account_check_number:
+            bankAccount.current_account_check_number,
+          holder_name: bankAccount.holder_name
+        },
+        credit_card: {
+          card_name: creditCard.card_name,
+          card_number: formatCreditCardRemoveSpace(creditCard.card_number),
+          expiration_date: creditCard.expiration_date,
+          cvc: creditCard.cvc
+        }
       },
-      bank_account: {
-        current_account: bankAccount.current_account,
-        current_account_check_number: bankAccount.current_account_check_number,
-        holder_name: bankAccount.holder_name
-      },
-      credit_card: {
-        card_name: creditCard.card_name,
-        card_number: creditCard.card_number.replace(/ /g, ''),
-        expiration_date: creditCard.expiration_date,
-        cvc: creditCard.cvc
+      {
+        onSuccess: () => setIsSuccessful(true),
+        onError: (err: AxiosError<ErrorResponse>) => {
+          const userHasBankAccountError =
+            err.response.data.statusCode === 409 &&
+            err.response.data.message ===
+              'This user already owns a bank account'
+
+          const unexpectedError =
+            err.response.status >= 400 &&
+            err.response.status < 500 &&
+            err.response.data.message !== 'This user already owns a bank account'
+
+          if (userHasBankAccountError) {
+            error('Este usuário já possui uma conta bancária cadastrada')
+          }
+
+          if (unexpectedError) {
+            setIsError(true)
+          }
+        }
       }
-    },
-    {
-      onSuccess: () => setIsSuccessful(true),
-      onError: () => setIsError(true)
-    })
+    )
   }
 
   return (
@@ -101,23 +119,33 @@ export function FinishedPage () {
                           />
                           Seu plano escolhido
                         </S.Title>
-                        <S.TitlePlan>{formatFirstLetterToUppercase(plan?.plan_name)}</S.TitlePlan>
+                        <S.TitlePlan>
+                          {formatFirstLetterToUppercase(plan?.plan_name)}
+                        </S.TitlePlan>
                         <S.CardDataContainer>
                           <S.CardDataTitle>Período de Cobrança</S.CardDataTitle>
                           <S.CardDataText>
                             {plan?.plan_duration === 6
                               ? 'Semestral'
-                              : (plan?.plan_duration === 1 ? 'Mensal' : 'Anual')}
+                              : plan?.plan_duration === 1
+                                ? 'Mensal'
+                                : 'Anual'}
                           </S.CardDataText>
                         </S.CardDataContainer>
                         <S.CardDataContainer>
                           <S.CardDataTitle>Total</S.CardDataTitle>
-                          <S.CardDataText>{formatPrice(plan?.price)}</S.CardDataText>
+                          <S.CardDataText>
+                            {formatPrice(plan?.price)}
+                          </S.CardDataText>
                         </S.CardDataContainer>
                       </S.Plan>
                       <S.Bank>
                         <S.Title>
-                          <BancoUm width='10.32' height='15' color={colors.mediumslateBlue} />
+                          <BancoUm
+                            width='10.32'
+                            height='15'
+                            color={colors.mediumslateBlue}
+                          />
                           Seu conta do Banco Um
                         </S.Title>
                         <S.CardDataContainer>
@@ -136,12 +164,18 @@ export function FinishedPage () {
                         </S.CardDataContainer>
                         <S.CardDataContainer>
                           <S.CardDataTitle>Titular</S.CardDataTitle>
-                          <S.CardDataText>{bankAccount?.holder_name}</S.CardDataText>
+                          <S.CardDataText>
+                            {bankAccount?.holder_name}
+                          </S.CardDataText>
                         </S.CardDataContainer>
                       </S.Bank>
                       <S.CreditCard>
                         <S.Title>
-                          <CreditCardIcon width='22' height='16' color={colors.mediumslateBlue} />
+                          <CreditCardIcon
+                            width='22'
+                            height='16'
+                            color={colors.mediumslateBlue}
+                          />
                           Seu cartão cadastrado
                         </S.Title>
                         <S.CardDataContainer>
@@ -158,7 +192,9 @@ export function FinishedPage () {
                         </S.CardDataContainer>
                         <S.CardDataContainer>
                           <S.CardDataTitle>Validade</S.CardDataTitle>
-                          <S.CardDataText>{creditCard?.expiration_date}</S.CardDataText>
+                          <S.CardDataText>
+                            {creditCard?.expiration_date}
+                          </S.CardDataText>
                         </S.CardDataContainer>
                       </S.CreditCard>
                       <S.ConfirmButton
@@ -170,24 +206,25 @@ export function FinishedPage () {
                         Finalizar
                       </S.ConfirmButton>
                       <Link href={CREDIT_CARD_PAGE}>
-                        <S.ReturnButton
-                          variant='secondary'
-                          type='button'
-                        >
+                        <S.ReturnButton variant='secondary' type='button'>
                           Voltar
                         </S.ReturnButton>
                       </Link>
                     </S.Form>
                   </>
                   )
-                : (<Successful
+                : (
+                  <Successful
                     paragraph='Seu cadastro foi finalizado com sucesso!
                      Aproveite as ofertas e Cashback no Clube Quantum!'
                     textTitle='Seja bem vindo!'
-                   />)}
+                  />
+                  )}
             </>
             )
-          : (<Error />)}
+          : (
+            <Error />
+            )}
       </S.Main>
       <Footer />
     </>
