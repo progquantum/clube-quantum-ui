47c12e63b83ec7e26b9b2a7c6b488b6fe6207519
itsrasfa <rafaelacontatoss@gmail.com>
2022-09-08 10:40:49 -0300

    refactor: add react-modal in context

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,14 +1,16 @@
 import { PropsWithChildren, useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
-
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { ToastContainer } from 'react-toastify'
 import { DefaultSeo } from 'next-seo'
+import Modal from 'react-modal'
 
 import SEO from '../../next-seo.config'
 
 import { AuthProvider } from './auth/AuthProvider'
 import { StyledProvider } from './styles'
+
+Modal.setAppElement('#__next')
 
 export function AppProvider ({ children }: PropsWithChildren<unknown>) {
   const [queryClient] = useState(() => new QueryClient())
