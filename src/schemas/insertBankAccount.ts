dbd6be831691e165cf55a6d7aef165e5e1358dd9
itsrasfa <rafaelacontatoss@gmail.com>
2022-09-08 10:42:13 -0300

    refactor: add holder name validation

--- src/schemas/insertBankAccount.ts
+++ src/schemas/insertBankAccount.ts
@@ -1,6 +1,6 @@
 import Yup from 'config/yup'
 
 export const schema = Yup.object().shape({
-  current_account: Yup.string().required().min(10),
-  holder_name: Yup.string().required()
+  current_account: Yup.string().required().min(10).max(10),
+  holder_name: Yup.string().required().matches(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/, 'O nome não deve conter caracteres especiais.')
 })
