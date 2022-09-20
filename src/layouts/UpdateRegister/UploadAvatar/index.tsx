c36f0653257e6379409576b2f39b6a1a6dc484c7
itsrasfa <rafaelacontatoss@gmail.com>
2022-09-20 11:49:50 -0300

    refactor: add component of UploadAvatarModal

--- src/layouts/UpdateRegister/UploadAvatar/index.tsx
+++ src/layouts/UpdateRegister/UploadAvatar/index.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react'
+
 import { Avatar } from 'components/Illustrations/Avatar'
 import { Edit } from 'components/Illustrations/Edit'
 
 import * as S from './styles'
+import { UploadAvatarModal } from './UploadAvatarModal'
 
 export function UploadAvatar () {
+  const [avatarModal, setAvatarModal] = useState(false)
+
+  const handleRequestAvatarModal = () => {
+    setAvatarModal(prevState => !prevState)
+  }
   return (
     <>
       <S.Container>
-        <S.EditAvatar>
+        <S.EditAvatar onClick={handleRequestAvatarModal}>
           <h2>Foto do Perfil</h2>
           <Edit width='15' height='15' color='#fff' />
         </S.EditAvatar>
         <Avatar />
+        <UploadAvatarModal isOpen={avatarModal} onRequestClose={handleRequestAvatarModal} />
       </S.Container>
     </>
   )
--- src/layouts/UpdateRegister/UploadAvatar/styles.ts
+++ src/layouts/UpdateRegister/UploadAvatar/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { Button } from 'components/Button'
+
 export const Container = styled.div`
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
   border-radius: .5rem;
@@ -11,13 +13,19 @@ export const Container = styled.div`
   padding: 1.25rem 1.5rem;
 `
 
-export const EditAvatar = styled.div`
+export const EditAvatar = styled(Button)`
   width: 100%;
   background: ${({ theme }) => theme.gradients.midnightBlueToMediumsLateBlue};
   padding: .9rem 1.8rem;
   border-radius: 6.25rem;
   display: flex;
   justify-content: space-between;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.mediumslateBlue};
+    transition: none;
+  }
 
   h2 {
     font-weight: 500;
