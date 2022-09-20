136558788f0b34a0d0bbd6955faea2281eeea793
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-09-20 17:55:08 -0300

    refactor: exchange image by component LeftWrapper

---
+++ src/layouts/SignUp/Forms/components/LeftWrapperPersonal/index.tsx
@@ -0,0 +1,24 @@
+import { useTheme } from 'styled-components'
+
+import { BancoUm } from 'components/Illustrations/BancoUm'
+
+import * as S from './styles'
+import { LeftWrapperProps } from './types'
+
+export function LeftWrapperPersonal ({ title, paragraph }:LeftWrapperProps) {
+  const { colors } = useTheme()
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.Text>{paragraph}</S.Text>
+      <BancoUm
+        color={colors.mediumaquamarine}
+        width='75'
+        height='45'
+      />
+      <S.SubTitle>Ainda não é um cliente Banco Um?</S.SubTitle>
+      <S.SubText>Conheça as vantagens de ser um cliente Banco Um</S.SubText>
+      <S.ButtonKnowMore>Saiba mais</S.ButtonKnowMore>
+    </S.Container>
+  )
+}
