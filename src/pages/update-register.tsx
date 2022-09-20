c8a54fde35f1788c9490e27d4d3033c41a9dea16
itsrasfa <rafaelacontatoss@gmail.com>
2022-09-20 11:53:35 -0300

    refactor: add auth to update-register page

--- src/pages/update-register.tsx
+++ src/pages/update-register.tsx
@@ -1,4 +1,13 @@
+import { GetServerSideProps } from 'next'
+
 import { UpdateRegisterPage } from 'layouts/UpdateRegister'
+import { withSSRAuth } from 'helpers/auth/withSSRAuth'
+
+export const getServerSideProps: GetServerSideProps = withSSRAuth(async () => {
+  return {
+    props: {}
+  }
+})
 
 export default function UpdateRegister () {
   return <UpdateRegisterPage />
