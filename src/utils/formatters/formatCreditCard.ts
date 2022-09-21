5f016ea36e375e8f19ffd6e97fecd141436e7476
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-09-21 11:20:58 -0300

    refactor: rename function and add new function remove space to formatCreditCard

--- src/utils/formatters/formatCreditCard.ts
+++ src/utils/formatters/formatCreditCard.ts
@@ -1,11 +1,11 @@
-// (/(.{4})/g, '$1 ') add right space for every 4 digits
-// /\s*$/remove all space in end of string
-
-export function formatCreditCard (value: string) {
+export function formatCreditCardAddSpace (value: string) {
   const valueFormatted = value.replace(/[^\d]/g, '')
 
   if (valueFormatted.length <= 19) {
     value = valueFormatted.replace(/(.{4})/g, '$1 ').replace(/\s*$/, '')
   }
   return value
+}
+export function formatCreditCardRemoveSpace (value: string) {
+  return value.replace(/ /g, '')
 }
