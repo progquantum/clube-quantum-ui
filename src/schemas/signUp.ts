7c6ca90ab38b22c0e5a039e1394f3855a16a9896
junior <junior@DESKTOP-MMJ56K0.localdomain>
2022-09-21 11:19:35 -0300

    refactor: add credcard and bankaccount yup schema

--- src/schemas/signUp.ts
+++ src/schemas/signUp.ts
@@ -105,3 +105,34 @@ export const addressDataSchema = Yup.object().shape({
     .required()
     .matches(/[A-Za-z]/g, 'Campo deve conter somente letras')
 })
+
+export const creditCardSchema = Yup.object().shape({
+  card_number: Yup
+    .string()
+    .required()
+    .matches(/^(4654)\s(47\d{2})\s(\d{4})\s(\d{4})$/, 'Cartão de crédito inválido!'),
+  card_name: Yup
+    .string()
+    .required(),
+  expiration_date: Yup
+    .string()
+    .required()
+    .matches(/(0[1-9]|10|11|12)[/](20\d{2})/, 'Validade do cartão inválida'),
+  cvc: Yup
+    .string()
+    .max(3)
+    .min(3)
+    .required()
+    .matches(/[0-9]/g, 'Campo deve conter somente números')
+})
+
+export const bankAccountSchema = Yup.object().shape({
+  current_account: Yup
+    .string()
+    .required()
+    .min(9)
+    .max(9),
+  holder_name: Yup
+    .string()
+    .required()
+})
