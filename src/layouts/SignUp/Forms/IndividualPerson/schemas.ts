0b98759ce9df0d339927f2cd1f4774194f7ea7f0
anajuliarauber <anajuliarauber@hotmail.com>
2022-10-06 14:15:52 -0300

    feat: validate birth date in signup

--- src/layouts/SignUp/Forms/IndividualPerson/schemas.ts
+++ src/layouts/SignUp/Forms/IndividualPerson/schemas.ts
@@ -1,8 +1,12 @@
+import { currentDate } from 'utils/birthDateValidation';
+
 import Yup from 'settings/yup';
 
 export const schema = Yup.object().shape({
   name: Yup.string().required(),
-  birth_date: Yup.date().required(),
+  birth_date: Yup.date()
+    .required()
+    .max(currentDate, 'Data de aniversário inválida'),
   email: Yup.string().email('Formato de email inválido').required(),
   email_confirmation: Yup.string()
     .required()
