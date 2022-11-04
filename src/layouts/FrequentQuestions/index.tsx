c91ab6aa82636e35e65e25c68f9684152e476ef6
itsrasfa <rafaelacontatoss@gmail.com>
2022-11-04 15:03:45 -0300

    feat: create FAQ page

---
+++ src/layouts/FrequentQuestions/index.tsx
@@ -0,0 +1,76 @@
+import Head from 'next/head';
+import { useState } from 'react';
+
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+import { BsHeadphones } from 'react-icons/bs';
+
+import { Header } from 'layouts/Home/Header';
+import { Footer } from 'components/Footer';
+
+import { data } from './questions';
+import * as S from './styles';
+
+export function FrequentQuestionsPage() {
+  const [questions, setQuestions] = useState(data);
+
+  const setIsShow = (id: number) => {
+    const newQuestions = questions.map(obj => {
+      if (obj.id === id) {
+        return { ...obj, show: !obj.show };
+      }
+      return obj;
+    });
+    setQuestions(newQuestions);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Perguntas Frequentes</title>
+      </Head>
+      <Header />
+      <S.Container>
+        <h1>Perguntas Frequentes</h1>
+        <p>Está com dúvidas? Aqui é o lugar!</p>
+        <S.QuestionContainer>
+          {questions?.map(question => (
+            <>
+              <S.Question key={question.id}>
+                <h3>{question.question}</h3>
+                {question.show ? (
+                  <MdKeyboardArrowDown
+                    size={20}
+                    onClick={() => setIsShow(question.id)}
+                  />
+                ) : (
+                  <MdKeyboardArrowUp
+                    size={20}
+                    onClick={() => setIsShow(question.id)}
+                  />
+                )}
+              </S.Question>
+              <S.Answer isShow={question.show}>{question.answer}</S.Answer>
+            </>
+          ))}
+        </S.QuestionContainer>
+        <S.Suport>
+          <S.SuportTitle>
+            <S.Chat>
+              <BsHeadphones size={30} />
+            </S.Chat>
+
+            <div>
+              <h2>Conseguimos te ajudar?</h2>
+              <p>
+                Não conseguiu achar a pergunta que estava procurando? Por favor
+                entre em contato com o nosso time! :)
+              </p>
+            </div>
+          </S.SuportTitle>
+          <S.SuportButton>Suporte</S.SuportButton>
+        </S.Suport>
+      </S.Container>
+      <Footer />
+    </>
+  );
+}
