b200bb3b77aea32435b69b97e143099d6ebab0ae
juniorvilas <log_junior@hotmail.com>
2022-11-07 12:13:50 -0300

    fix: fix a bug in token and refresh token requests

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -49,20 +49,9 @@ export function queueInstance(
 
   const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_HOST,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
   });
 
-  api.interceptors.request.use(
-    config => {
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-      return config;
-    },
-
-    error => Promise.reject(error),
-  );
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
 
   api.interceptors.response.use(
     response => response,
