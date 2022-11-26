99c207a6cc1901562fa172f1fc60d950db8b79f3
itsrasfa <rafaelacontatoss@gmail.com>
2022-11-26 00:28:44 -0300

    refactor: update my friends page

--- src/layouts/MyFriends/index.tsx
+++ src/layouts/MyFriends/index.tsx
@@ -1,21 +1,158 @@
-import { Footer } from 'components/Footer';
-import { Header } from 'components/Header';
-import { ServicesBank } from 'components/ServicesBank';
-import { SideBar } from 'components/SideBar';
+import { RiUserStarLine } from 'react-icons/ri';
+import { BsFillPersonPlusFill } from 'react-icons/bs';
+
+import ReactPaginate from 'react-paginate';
+
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+
+import { formatCashback } from 'utils/formatters/formatCashback';
+
+import { useFriends } from 'hooks/useFriends';
+
+import { DashboardLayout } from 'layouts/DashboardLayout';
+
+import { InviteFriends } from 'components/InviteFriends';
+
+import { colors } from 'styles/theme/colors';
 
 import * as S from './styles';
 
 export function MyFriendsPage() {
+  const { data, onPageChange } = useFriends();
+
+  const totalPages = data?.totalPages;
+
   return (
-    <>
-      <Header />
-      <S.Container>
-        <SideBar />
-        <S.RightWrapper>
-          <ServicesBank />
-        </S.RightWrapper>
-      </S.Container>
-      <Footer />
-    </>
+    <DashboardLayout>
+      <S.CardsContainer>
+        <S.CardFriends>
+          <S.CardTitle>
+            <RiUserStarLine size={16} /> Amigos
+          </S.CardTitle>
+          {data?.friends.map(friend => (
+            <S.Friend key={friend.id}>
+              <S.TitleFriends>
+                <S.Image src={friend.avatar_url} alt="" />
+                <div>
+                  <h5>{friend.name}</h5>
+                  {friend.inactivated_at !== null ? (
+                    <span>Inativo(a) desde {friend.inactivated_at}</span>
+                  ) : (
+                    <div>
+                      {friend.cashback !== 0 ? (
+                        <S.Cashback>
+                          {formatCashback(friend.cashback)}
+                        </S.Cashback>
+                      ) : (
+                        <S.NoCashback>R$ 0,00</S.NoCashback>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </S.TitleFriends>
+              <S.AmountFriends>
+                <BsFillPersonPlusFill size={16} />
+                <p>{friend.amountFriends}</p>
+              </S.AmountFriends>
+            </S.Friend>
+          ))}
+          <S.PaginationContainer>
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel={
+                <IoIosArrowForward size={20} color={colors.mediumslateBlue} />
+              }
+              onPageChange={onPageChange}
+              pageCount={totalPages}
+              previousLabel={
+                <IoIosArrowBack size={20} color={colors.mediumslateBlue} />
+              }
+              containerClassName="paginationContainer"
+              pageLinkClassName="pageLink"
+              activeLinkClassName="activeLink"
+            />
+          </S.PaginationContainer>
+        </S.CardFriends>
+
+        <S.FlexCards>
+          <S.IndirectFriends>
+            <S.CardTitle>
+              <RiUserStarLine size={16} /> Ganhos por indicações indiretas
+            </S.CardTitle>
+
+            <S.TotalFriends>
+              <p>Suas conexões indiretas</p>
+              <span>167/250</span>
+            </S.TotalFriends>
+
+            <progress value="75" max="100" />
+
+            <S.TotalFriends>
+              <p>
+                Seu ganho com conexões <br /> indiretas esse mês foi de:
+              </p>
+              <span>R$ 12,00</span>
+            </S.TotalFriends>
+          </S.IndirectFriends>
+
+          <S.IndirectFriends>
+            <S.CardTitle>
+              <RiUserStarLine size={16} /> Ganhos por indicações indiretas
+            </S.CardTitle>
+            <S.GraphicBar>
+              <S.BarItem>
+                <S.Bar percentage={100} />
+                <S.TitleBar>Jan</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={50} />
+                <S.TitleBar>Fev</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={70} />
+                <S.TitleBar>Mar</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={40} />
+                <S.TitleBar>Abr</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={40} />
+                <S.TitleBar>Mai</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={80} />
+                <S.TitleBar>Jun</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={90} />
+                <S.TitleBar>Jul</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={49} />
+                <S.TitleBar>Ago</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={100} />
+                <S.TitleBar>Set</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={38} />
+                <S.TitleBar>Out</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={73} />
+                <S.TitleBar>Nov</S.TitleBar>
+              </S.BarItem>
+              <S.BarItem>
+                <S.Bar percentage={87} />
+                <S.TitleBar>Dez</S.TitleBar>
+              </S.BarItem>
+            </S.GraphicBar>
+          </S.IndirectFriends>
+          <InviteFriends variant="white" />
+        </S.FlexCards>
+      </S.CardsContainer>
+    </DashboardLayout>
   );
 }
--- src/layouts/MyFriends/styles.ts
+++ src/layouts/MyFriends/styles.ts
@@ -1,21 +1,205 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
-  max-width: 72.125rem;
-  width: 100%;
-  margin: 5rem auto;
-  gap: 3.125rem;
-  color: ${({ theme }) => theme.colors.gray[700]};
+import { BarProps } from './types';
+
+export const CardsContainer = styled.div`
   display: flex;
-  align-items: flex-start;
-  justify-content: space-evenly;
+  justify-content: space-between;
+  margin: 2rem 0;
+  gap: 1rem;
+
+  @media (max-width: 1024px) {
+    justify-content: center;
+    gap: 1rem;
+    flex-direction: column;
+  }
 `;
 
-export const RightWrapper = styled.div`
+export const CardFriends = styled.div`
+  flex: 1;
+  box-shadow: 0rem 0rem 2rem rgba(41, 40, 40, 0.1);
+  width: 100%;
+  border-radius: 0.625rem;
+  padding: 1rem 1.5rem;
   display: flex;
   flex-direction: column;
+  gap: 1.875rem;
+`;
 
-  @media (max-width: 460px) {
-    align-items: center;
+export const CardTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  color: ${({ theme }) => theme.colors.gray['400']};
+  font-weight: 500;
+  font-size: 0.625rem;
+  line-height: 1.25rem;
+  margin-bottom: 0.5rem;
+
+  > svg {
+    color: ${({ theme }) => theme.colors.gray['200']};
   }
+`;
+
+export const Friend = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  h5 {
+    font-weight: 600;
+    font-size: 0.75rem;
+    margin-bottom: 0.4rem;
+  }
+
+  span {
+    font-size: 0.7rem;
+    color: ${({ theme }) => theme.colors.gray[400]};
+  }
+`;
+
+export const Cashback = styled.p`
+  font-weight: 600;
+  font-size: 0.7rem;
+  color: #0a7003;
+`;
+
+export const NoCashback = styled.p`
+  font-weight: 600;
+  font-size: 0.7rem;
+  color: ${({ theme }) => theme.colors.gray[300]};
+`;
+
+export const TitleFriends = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const AmountFriends = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.colors.mediumslateBlue};
+  font-size: 0.8rem;
+
+  p {
+    color: ${({ theme }) => theme.colors.mediumslateBlue};
+    font-weight: 500;
+  }
+
+  &::before {
+    display: block;
+    content: '';
+    width: 2px;
+    height: 20px;
+    background-color: ${({ theme }) => theme.colors.gray[50]};
+    margin-right: 10px;
+    border-radius: 5px;
+  }
+`;
+
+export const Image = styled.img`
+  width: 2.2rem;
+  height: 2.2rem;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+export const FlexCards = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const IndirectFriends = styled.div`
+  width: 100%;
+  box-shadow: 0rem 0rem 2rem rgba(41, 40, 40, 0.1);
+  border-radius: 0.625rem;
+  padding: 1rem 1.5rem;
+
+  progress {
+    width: 100%;
+    -webkit-appearance: none;
+    appearance: none;
+    border: none;
+    border-radius: 0.5rem;
+    height: 0.9375rem;
+    margin: 1rem 0;
+
+    &::-webkit-progress-bar,
+    &::-webkit-progress-value {
+      border-radius: 0.625rem;
+    }
+
+    &::-webkit-progress-bar {
+      background: ${({ theme }) => theme.colors.lightsteelblueice};
+    }
+
+    &::-webkit-progress-value {
+      background: ${({ theme }) =>
+        theme.gradients.mediumsLateBlueToMidnightBlue};
+    }
+  }
+`;
+
+export const TotalFriends = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.colors.midnightBlue};
+  margin: 1rem 0;
+
+  p {
+    font-size: 0.8rem;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 1rem;
+  }
+`;
+
+export const PaginationContainer = styled.div`
+  margin: 1rem 0;
+`;
+
+export const GraphicBar = styled.div`
+  width: 100%;
+  height: 6.6875rem;
+  display: flex;
+  gap: 0.4375rem;
+  justify-content: space-between;
+  margin-top: 1rem;
+
+  @media (max-width: 380px) {
+    gap: 0;
+  }
+`;
+
+export const BarItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.375rem;
+`;
+
+export const Bar = styled.div<BarProps>`
+  height: ${({ percentage }) => percentage}%;
+  width: 10px;
+  border-radius: 50px;
+  background: ${({ theme }) => theme.gradients.midnightBlueToMediumsLateBlue};
+  position: relative;
+  display: inline-block;
+  &:hover span {
+    visibility: visible;
+  }
+`;
+
+export const TitleBar = styled.h3`
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 20px;
+  color: ${({ theme }) => theme.colors.midnightBlue};
+  transform: rotate(-45deg);
 `;
