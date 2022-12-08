acb7fb62292a7e50060bf52c78d34b95de590079
Caio <caiocastrofrs@gmail.com>
2022-12-08 17:11:29 -0300

    refactor: remove loading animation on dashboard page

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable no-nested-ternary */
-import { Loader } from 'components/Loader';
 import { ManagePlans } from 'components/ManagePlans';
 
 import { useMe } from 'hooks/user/useMe';
@@ -10,18 +9,14 @@ import { DashboardLayout } from 'layouts/DashboardLayout';
 import { MainContent } from './MainContent/Index';
 
 export function DashboardPage() {
-  const { data, isLoading } = useMe();
+  const { data } = useMe();
 
   return (
     <>
       <title>Dashboard - Clube Quantum</title>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <DashboardLayout>
-          {data?.subscription ? <MainContent /> : <ManagePlans />}
-        </DashboardLayout>
-      )}
+      <DashboardLayout>
+        {data?.subscription ? <MainContent /> : <ManagePlans />}
+      </DashboardLayout>
     </>
   );
 }
