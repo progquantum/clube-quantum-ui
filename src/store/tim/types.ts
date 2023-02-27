50200bd7e63dcd787355df571ccd22b7c1b02094
Caio <caiocastrofrs@gmail.com>
2023-02-27 11:42:37 -0300

    feat: add zustand dependency and configure tim store

---
+++ src/store/tim/types.ts
@@ -0,0 +1,19 @@
+import { Plan } from 'layouts/TimSubscriptionPlan/Steps/PlanSection/TimPlan/types';
+
+export type TimPlanStore = {
+  selectedPlan: Plan;
+  setPlan: (plan: Plan) => void;
+  currentStep: number;
+  nextStep: () => void;
+  previousStep: () => void;
+  whichPath: 'pathOne' | 'pathTwo' | 'default';
+  setPath: (path: string) => void;
+  selectedDDD: string;
+  setDDD: (ddd: string) => void;
+  phoneNumber: string;
+  setPhoneNumber: (phone: string) => void;
+  pinCode: string;
+  setPinCode: (pinCode: string) => void;
+  isPaymentFailed: boolean;
+  setPaymentToggle: () => void;
+};
