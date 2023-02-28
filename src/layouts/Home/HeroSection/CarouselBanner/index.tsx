82620b0d893e62bd26af4ec27e0dc916c1db4c31
juniorvilas <log_junior@hotmail.com>
2023-02-28 10:35:13 -0300

    refactor: add state to set previousPage

--- src/layouts/Home/HeroSection/CarouselBanner/index.tsx
+++ src/layouts/Home/HeroSection/CarouselBanner/index.tsx
@@ -7,14 +7,18 @@ import {
   PLAN_TIM_PAGE,
   POS_PAGE,
 } from 'constants/routesPath';
+import { useAuthDispatch } from 'contexts/auth/AuthContext';
 
 export function CarouselBanner() {
+  const { setPreviousPage } = useAuthDispatch();
   return (
     <Carousel>
       <Carousel.Item>
         <div style={{ margin: ' 50px 0px' }}>
           <Link href={POS_PAGE} prefetch>
             <Image
+              onClick={() => setPreviousPage(POS_PAGE)}
+              style={{ cursor: 'pointer' }}
               src="/images/banner_pos.svg"
               alt="Máquina Pos"
               width="1280px"
@@ -27,6 +31,8 @@ export function CarouselBanner() {
         <div style={{ margin: ' 50px 60px' }}>
           <Link href={MARKETPLACE_PAGE} prefetch>
             <Image
+              onClick={() => setPreviousPage(MARKETPLACE_PAGE)}
+              style={{ cursor: 'pointer' }}
               src="/images/banner_marketplace.svg"
               alt="Marketplace"
               width="1200px"
@@ -39,6 +45,8 @@ export function CarouselBanner() {
         <div style={{ margin: ' 50px 60px' }}>
           <Link href={PLAN_TIM_PAGE} prefetch>
             <Image
+              onClick={() => setPreviousPage(PLAN_TIM_PAGE)}
+              style={{ cursor: 'pointer' }}
               src="/images/banner_tim.svg"
               alt="Banner Tim + Banco um"
               width="1200px"
