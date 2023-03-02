47859e6355aeac5061bac9c2cebb22011a064576
Caio <caiocastrofrs@gmail.com>
2023-03-02 11:57:58 -0300

    refactor: invite friends component styles

--- src/components/InviteFriends/index.tsx
+++ src/components/InviteFriends/index.tsx
@@ -1,30 +1,76 @@
-import Link from 'next/link';
+import { BsInstagram, BsPersonBadge } from 'react-icons/bs';
 
-import { RiUserStarLine } from 'react-icons/ri';
+import { FaFacebookF } from 'react-icons/fa';
 
-import { INVITE_FRIENDS_PAGE } from 'constants/routesPath';
+import { ImWhatsapp } from 'react-icons/im';
+
+import { useMe } from 'hooks/user/useMe';
+
+import { useFriends } from 'hooks/useFriends';
 
 import * as S from './styles';
 import { InviteFriendsProps } from './types';
 
 export function InviteFriends({ variant }: InviteFriendsProps) {
+  const {
+    data: { invite_code },
+  } = useMe();
+  const { data } = useFriends();
+
+  const totalFriends = (data && data.friends.length) ?? 0;
+
+  const inviteLink = `quantum.com.vc/signup?invite-code=${invite_code}`;
+
+  const handleCopyOnClick = async () => {
+    try {
+      await navigator.clipboard.writeText(inviteLink);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return err;
+      }
+    }
+  };
+
   return (
     <S.DivInviteFriends variant={variant}>
-      <S.HeaderInviteFriends variant={variant}>
-        <RiUserStarLine />
-        <S.TitleFriends variant={variant}>Convidar amigos</S.TitleFriends>
+      <S.HeaderInviteFriends>
+        <BsPersonBadge />
+        <S.TitleFriends>Convidar amigos</S.TitleFriends>
       </S.HeaderInviteFriends>
-      <S.TitleInviteFriends variant={variant}>
-        Não fique sozinho nessa!
-      </S.TitleInviteFriends>
-      <S.TextInviteFriends variant={variant}>
+      <S.TitleInviteFriends>Não fique sozinho nessa!</S.TitleInviteFriends>
+      <S.TextInviteFriends>
         Convide seus amigos e ganhe cashback junto com eles!!!
       </S.TextInviteFriends>
-      <Link href={INVITE_FRIENDS_PAGE}>
-        <S.ButtonInviteFriends variant={variant}>
-          Convidar amigos
-        </S.ButtonInviteFriends>
-      </Link>
+      <S.InviteBox onClick={handleCopyOnClick}>
+        Seu link de convite é: <S.Bold>{inviteLink}</S.Bold>{' '}
+        <span>clique aqui para copiar!</span>
+      </S.InviteBox>
+      <S.SocialIconsBox>
+        <div>
+          <BsInstagram size={32} />
+        </div>
+        <div>
+          <FaFacebookF size={32} />
+        </div>
+        <div>
+          <ImWhatsapp size={32} />
+        </div>
+      </S.SocialIconsBox>
+      <S.InvitationsAcceptedBox>
+        <div>
+          <span>Convites aceitos</span>
+          <span>{totalFriends ?? 0}/250</span>
+        </div>
+        <S.ProgressBar
+          quantityFilledInPercent={
+            totalFriends > 250
+              ? '100%'
+              : String((totalFriends / 250) * 100).concat('%')
+          }
+        >
+          <div />
+        </S.ProgressBar>
+      </S.InvitationsAcceptedBox>
     </S.DivInviteFriends>
   );
 }
