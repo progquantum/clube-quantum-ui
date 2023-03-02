883bdc8f2d3cd67dc979e0ce46217c64fee5cf58
Caio <caiocastrofrs@gmail.com>
2023-03-02 11:59:02 -0300

    refactor: sidebar layout

--- src/components/SideBar/types.ts
+++ src/components/SideBar/types.ts
@@ -1,6 +1,10 @@
+export type IsExpanded = {
+  isExpanded: boolean;
+};
 export type NavButtonProps = {
   activePath?: boolean;
-};
+} & IsExpanded;
+
 export type SideBarProps = {
   loading?: boolean;
 };
