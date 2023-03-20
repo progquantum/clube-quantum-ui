4dce2fc3a767b547bc16721c835f6c29126e3c18
Caio Castro <caio.castro@arloestitech.com.br>
2023-03-20 16:53:23 -0300

    feat: add list of code to populate select options

---
+++ src/layouts/TimSubscriptionPlan/Steps/WhichNumber/PathTwo/listOfDDDs.ts
@@ -0,0 +1,204 @@
+const ddds_SP = [
+  { value: undefined, title: 'São Paulo' },
+  { value: '11', title: 'DDD 11' },
+  { value: '12', title: 'DDD 12' },
+  { value: '13', title: 'DDD 13' },
+  { value: '14', title: 'DDD 14' },
+  { value: '15', title: 'DDD 15' },
+  { value: '16', title: 'DDD 16' },
+  { value: '17', title: 'DDD 17' },
+  { value: '18', title: 'DDD 18' },
+  { value: '19', title: 'DDD 19' },
+];
+
+const ddds_RJ = [
+  { value: undefined, title: 'Rio de Janeiro' },
+  { value: '21', title: 'DDD 21' },
+  { value: '22', title: 'DDD 22' },
+  { value: '24', title: 'DDD 24' },
+];
+
+const ddds_ES = [
+  { value: undefined, title: 'Espirito Santo' },
+  { value: '27', title: 'DDD 27' },
+  { value: '28', title: 'DDD 28' },
+];
+
+const ddds_MG = [
+  { value: undefined, title: 'Minas Gerais' },
+  { value: '31', title: 'DDD 31' },
+  { value: '32', title: 'DDD 32' },
+  { value: '33', title: 'DDD 33' },
+  { value: '34', title: 'DDD 34' },
+  { value: '35', title: 'DDD 35' },
+  { value: '37', title: 'DDD 37' },
+  { value: '38', title: 'DDD 38' },
+];
+
+const ddds_PR = [
+  { value: undefined, title: 'Paraná' },
+  { value: '41', title: 'DDD 41' },
+  { value: '42', title: 'DDD 42' },
+  { value: '43', title: 'DDD 43' },
+  { value: '44', title: 'DDD 44' },
+  { value: '45', title: 'DDD 45' },
+  { value: '46', title: 'DDD 46' },
+];
+
+const ddds_SC = [
+  { value: undefined, title: 'Santa Catarina' },
+  { value: '47', title: 'DDD 47' },
+  { value: '48', title: 'DDD 48' },
+  { value: '49', title: 'DDD 49' },
+];
+
+const ddds_RS = [
+  { value: undefined, title: 'Rio Grande do Sul' },
+  { value: '51', title: 'DDD 51' },
+  { value: '53', title: 'DDD 53' },
+  { value: '54', title: 'DDD 54' },
+  { value: '55', title: 'DDD 55' },
+];
+
+const ddds_AC = [
+  { value: undefined, title: 'Acre' },
+  { value: '68', title: 'DDD 68' },
+];
+
+const ddds_AP = [
+  { value: undefined, title: 'Amapá' },
+  { value: '96', title: 'DDD 96' },
+];
+
+const ddds_AM = [
+  { value: undefined, title: 'Amazonas' },
+  { value: '92', title: 'DDD 92' },
+  { value: '97', title: 'DDD 97' },
+];
+
+const ddds_PA = [
+  { value: undefined, title: 'Pará' },
+  { value: '91', title: 'DDD 91' },
+  { value: '93', title: 'DDD 93' },
+  { value: '94', title: 'DDD 94' },
+];
+
+const ddds_RO = [
+  { value: undefined, title: 'Rondônia' },
+  { value: '69', title: 'DDD 69' },
+];
+
+const ddds_RR = [
+  { value: undefined, title: 'Roraima' },
+  { value: '95', title: 'DDD 95' },
+];
+
+const ddds_TO = [
+  { value: undefined, title: 'Tocantins' },
+  { value: '63', title: 'DDD 63' },
+];
+
+const ddds_AL = [
+  { value: undefined, title: 'Alagoas' },
+  { value: '82', title: 'DDD 82' },
+];
+
+const ddds_BA = [
+  { value: undefined, title: 'Bahia' },
+  { value: '71', title: 'DDD 71' },
+  { value: '73', title: 'DDD 73' },
+  { value: '74', title: 'DDD 74' },
+  { value: '75', title: 'DDD 75' },
+  { value: '77', title: 'DDD 77' },
+];
+
+const ddds_CE = [
+  { value: undefined, title: 'Ceará' },
+  { value: '85', title: 'DDD 85' },
+  { value: '88', title: 'DDD 88' },
+];
+
+const ddds_MA = [
+  { value: undefined, title: 'Maranhão' },
+  { value: '98', title: 'DDD 98' },
+  { value: '99', title: 'DDD 99' },
+];
+
+const ddds_PB = [
+  { value: undefined, title: 'Paraíba' },
+  { value: '83', title: 'DDD 83' },
+];
+
+const ddds_PE = [
+  { value: undefined, title: 'Pernambuco' },
+  { value: '81', title: 'DDD 81' },
+  { value: '87', title: 'DDD 87' },
+];
+
+const ddds_PI = [
+  { value: undefined, title: 'Piauí' },
+  { value: '86', title: 'DDD 86' },
+  { value: '89', title: 'DDD 89' },
+];
+
+const ddds_RN = [
+  { value: undefined, title: 'Rio Grande do Norte' },
+  { value: '84', title: 'DDD 84' },
+];
+
+const ddds_SE = [
+  { value: undefined, title: 'Sergipe' },
+  { value: '79', title: 'DDD 79' },
+];
+
+const ddds_DF = [
+  { value: undefined, title: 'Distrito Federal' },
+  { value: '61', title: 'DDD 61' },
+];
+
+const ddds_GO = [
+  { value: undefined, title: 'Goiás' },
+  { value: '62', title: 'DDD 62' },
+  { value: '64', title: 'DDD 64' },
+];
+
+const ddds_MT = [
+  { value: undefined, title: 'Mato Grosso' },
+  { value: '65', title: 'DDD 65' },
+  { value: '66', title: 'DDD 66' },
+];
+
+const ddds_MS = [
+  { value: undefined, title: 'Mato Grosso do Sul' },
+  { value: '67', title: 'DDD 67' },
+];
+
+export const ddds = [
+  ...ddds_SP,
+  ...ddds_RJ,
+  ...ddds_ES,
+  ...ddds_MG,
+  ...ddds_PR,
+  ...ddds_SC,
+  ...ddds_RS,
+  ...ddds_DF,
+  ...ddds_GO,
+  ...ddds_MT,
+  ...ddds_MS,
+  ...ddds_TO,
+  ...ddds_AC,
+  ...ddds_BA,
+  ...ddds_SE,
+  ...ddds_PE,
+  ...ddds_PB,
+  ...ddds_PI,
+  ...ddds_RN,
+  ...ddds_AL,
+  ...ddds_CE,
+  ...ddds_MA,
+  ...ddds_AP,
+  ...ddds_AM,
+  ...ddds_PA,
+  ...ddds_RO,
+  ...ddds_RR,
+];
