d8221d796c22494cdc14486d6a3efec01dbfb135
Caio Castro <caio.castro@arloesitech.com.br>
2023-04-05 15:08:55 -0300

    refactor: change filter categories layout

--- src/layouts/Marketplace/FilterTags/Tag/index.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import * as S from './styles';
-
-export function Tag({ children }: PropsWithChildren) {
-  return <S.TagContainer>{children}</S.TagContainer>;
-}
