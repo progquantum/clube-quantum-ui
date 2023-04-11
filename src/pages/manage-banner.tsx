be60d1016e5739c962815c091c1f0387d61ccaeb
Caio Castro <caio.castro@arloesitech.com.br>
2023-04-11 11:23:19 -0300

    feat: add manage banner to authenticated routes

--- src/pages/manage-banner.tsx
+++ src/pages/manage-banner.tsx
@@ -1,8 +1,11 @@
+import { GetServerSideProps } from 'next';
+
+import { withSSRAuth } from 'helpers/auth/withSSRAuth';
 import { ManageBannerPage } from 'layouts/ManageBanner';
 
-export async function getServerSideProps() {
-  return { props: {} };
-}
+export const getServerSideProps: GetServerSideProps = withSSRAuth(async () => ({
+  props: {},
+}));
 
 export default function ManageBanner() {
   return <ManageBannerPage />;
