a682d2c0e3d922302194a278e767b4b17c09feaa
Caio Castro <caio.castro@arloesitech.com.br>
2023-04-14 17:20:45 -0300

    feat: add hook to get all contracts of logged user

---
+++ src/hooks/useContracts/useFindContractByUserId/index.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from 'axios';
+
+import { useQuery } from 'react-query';
+
+import { quantumClientQueue } from 'config/client';
+
+import { ResponseData } from './types';
+
+const QUERY_KEY_GET_CONTRACTS_LOGGED_USER = 'get-contracts-logged-user';
+
+export async function getContractsLoggedUser() {
+  try {
+    const { data } = await quantumClientQueue.get<ResponseData>(
+      '/contracts/find-contract-by-user_id',
+    );
+
+    return data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      console.log(error);
+    }
+    return Promise.reject(error);
+  }
+}
+
+export function useGetContractsLoggedUser() {
+  return useQuery(
+    [QUERY_KEY_GET_CONTRACTS_LOGGED_USER],
+    getContractsLoggedUser,
+  );
+}
