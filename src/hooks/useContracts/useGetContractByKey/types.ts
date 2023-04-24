5baf956dd5bebd6b7e349c3e588037cc9763a4c5
Caio Castro <caio.castro@arloesitech.com.br>
2023-04-24 15:57:41 -0300

    feat: add hook to get contract data using document key

---
+++ src/hooks/useContracts/useGetContractByKey/types.ts
@@ -0,0 +1,19 @@
+export type PersonalInformation = {
+  name: string;
+  birth_date: Date;
+  email: string;
+};
+
+export type ContractInformation = {
+  name: string;
+  plan_name: string;
+  date_of_acquisition: Date;
+  monthly_fee: number;
+  phone_number: string;
+  area_code: string;
+};
+
+export type ResponseData = {
+  personal_information: PersonalInformation;
+  contract_information: ContractInformation;
+};
