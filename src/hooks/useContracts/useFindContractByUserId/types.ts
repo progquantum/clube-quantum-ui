dc51078aa5995cbedf0ad8b436c059e80613f3d1
Caio Castro <caio.castro@arloesitech.com.br>
2023-05-29 11:27:00 -0300

    feat: update type and response data on Find Contract By Id

--- src/hooks/useContracts/useFindContractByUserId/types.ts
+++ src/hooks/useContracts/useFindContractByUserId/types.ts
@@ -5,6 +5,10 @@ export type Contract = {
   plan_name: string;
   date_of_acquisition: string;
   file_name: string;
+  marketplace_subscription_id: string;
+  justification: string;
+  cancelled_at: string;
+  cancelled_status: string;
 };
 
 export type ResponseData = Contract[];
