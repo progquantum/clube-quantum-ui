f3d7c45518d6d6454cb52aad792446e78de5aa84
juniorvilas <log_junior@hotmail.com>
2023-06-02 15:34:23 -0300

    feat: create new hook useDeleteBanners

---
+++ src/hooks/banners/useDeleteBanners/index.ts
@@ -0,0 +1,21 @@
+import { useMutation } from 'react-query';
+import { AxiosError } from 'axios';
+
+import { quantumClientQueue } from 'config/client';
+import { error } from 'helpers/notify/error';
+
+export const bannerDeleteRequest = async (id: string) => {
+  try {
+    await quantumClientQueue.delete(`/banners/${id}`);
+  } catch (err: unknown) {
+    if (err instanceof AxiosError) {
+      if (err.response.status === 404) {
+        error('Id da imagem não encontrado');
+      }
+    }
+  }
+};
+
+export function useDeleteBanners(id: string) {
+  return useMutation(() => bannerDeleteRequest(id));
+}
