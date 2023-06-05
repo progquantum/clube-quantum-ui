ed3541c1c5e7039ddd1ed9cd02640a082d920275
juniorvilas <log_junior@hotmail.com>
2023-06-05 15:32:28 -0300

    refactor: add logic to update banner

--- src/layouts/ManageBanner/UploadPhotoBox/index.tsx
+++ src/layouts/ManageBanner/UploadPhotoBox/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import Image from 'next/image';
 import { MdClose } from 'react-icons/md';
+import { FiEdit3 } from 'react-icons/fi';
 
 import { useQueryClient } from 'react-query';
 
@@ -27,6 +29,7 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
   const { mutateAsync: uploadBanner, isLoading: loading } = useBannersUpload();
   const { mutate: deleteImg } = useDeleteBanners(data?.id);
   const [isImgAvatar, setImgPreview] = useState<string>('');
+  const [isIdPreview, setIdPreview] = useState<string>('');
   const [imgUploaded, setImgUploaded] = useState(null);
   const [inputKey, setInputKey] = useState(0);
   const queryClient = useQueryClient();
@@ -36,6 +39,7 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
     if (data === undefined) {
       setImgPreview('');
       setImgUploaded(null);
+      setIdPreview('');
     }
   }, []);
 
@@ -61,7 +65,7 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
     if (!allowedImageFormats) return error('Arquivo não suportado');
   };
 
-  const handleUpdateImage = async (e: FormEvent) => {
+  const handleCreateImage = async (e: FormEvent) => {
     e.preventDefault();
 
     await uploadBanner(
@@ -74,6 +78,26 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
           setImgPreview('');
           setImgUploaded(null);
           success('Banner enviado com sucesso!');
+        },
+      },
+    );
+  };
+
+  const handleUpdateImage = async (e: FormEvent, id: string) => {
+    e.preventDefault();
+
+    await uploadBanner(
+      {
+        file: imgUploaded,
+        image_id: id,
+      },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(QUERY_KEY_BANNERS_FIND_ALL);
+          setImgPreview('');
+          setImgUploaded(null);
+          setIdPreview('');
+          success('Banner atualizado com sucesso!');
         },
       },
     );
@@ -98,23 +122,73 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
   const removePreview = () => {
     setImgPreview('');
     setImgUploaded(null);
+    setIdPreview('');
   };
 
   if (data) {
     return (
-      <S.UploadPhotoBox style={{ padding: '0' }}>
-        <Image
-          src={data.url}
-          width={width < 375 ? '310' : '357'}
-          height="203"
-          priority
-          alt="banner image"
-        />
-        <MdClose
-          style={{ position: 'absolute' }}
-          size={24}
-          onClick={() => removeBannerFromArray()}
-        />
+      <S.UploadPhotoBox
+        style={{
+          padding: isImgAvatar && isIdPreview === data.id ? undefined : '0',
+        }}
+      >
+        {isImgAvatar && isIdPreview === data.id ? (
+          <Image src={isImgAvatar} width={250} height={131} alt="img preview" />
+        ) : (
+          <>
+            <Image
+              src={data.url}
+              width={width < 375 ? '310' : '357'}
+              height="203"
+              priority
+              alt="banner image"
+            />
+            <MdClose
+              style={{ position: 'absolute' }}
+              size={24}
+              onClick={() => removeBannerFromArray()}
+            />
+            <div className="edit">
+              <label htmlFor={`update${data.id}`}>
+                <FiEdit3 size={24} />
+              </label>
+              <S.ActualFileInput
+                type="file"
+                name={`update${data.id}`}
+                id={`update${data.id}`}
+                key={inputKey}
+                onChange={e => {
+                  handleChangePreview(e);
+                  setIdPreview(data.id);
+                }}
+              />
+            </div>
+          </>
+        )}
+
+        {isImgAvatar && isIdPreview === data.id && (
+          <>
+            <Button
+              type="submit"
+              loading={loading}
+              disabled={loading}
+              style={{
+                width: '135px',
+                height: '26px',
+                borderRadius: '8px',
+                marginTop: '10px',
+              }}
+              onClick={e => handleUpdateImage(e, data.id)}
+            >
+              Salvar
+            </Button>
+            <MdClose
+              style={{ position: 'absolute' }}
+              size={24}
+              onClick={() => removePreview()}
+            />
+          </>
+        )}
       </S.UploadPhotoBox>
     );
   }
@@ -144,7 +218,7 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
               borderRadius: '8px',
               marginTop: '10px',
             }}
-            onClick={e => handleUpdateImage(e)}
+            onClick={e => handleCreateImage(e)}
           >
             Salvar
           </Button>
@@ -156,7 +230,7 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
               flexDirection: 'column',
             }}
           >
-            <S.FileInputButton htmlFor="upload">
+            <S.FileInputButton htmlFor="create">
               Adicionar fotos
             </S.FileInputButton>
             <span>Dimensão: 642px X 321px</span>
@@ -165,8 +239,8 @@ export function UploadPhotoBox({ data }: UploadPhotoBoxProps) {
         )}
         <S.ActualFileInput
           type="file"
-          name="upload"
-          id="upload"
+          name="create"
+          id="create"
           key={inputKey}
           onChange={e => handleChangePreview(e)}
         />
