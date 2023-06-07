d7d19ec16bafb0a208a59b0aa99c12814bd8ee7d
Caio Castro <caio.castro@arloesitech.com.br>
2023-06-07 17:30:25 -0300

    refactor: decrease the spacing between service components and add box-shadow

--- src/layouts/Marketplace/Services/styles.ts
+++ src/layouts/Marketplace/Services/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const ServiceContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 3rem;
+  gap: 1rem;
   margin: 3rem 0;
+
   @media (max-width: 447px) {
     justify-content: center;
     align-items: center;
