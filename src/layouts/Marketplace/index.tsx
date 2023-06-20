85f6cb28dc069f5d3acbdc6b3b2d4968710ca05b
Caio Castro <caio.castro@arloesitech.com.br>
2023-06-20 17:57:30 -0300

    refactor: change marketplace mobile banner to be the same banner from homepage

--- src/layouts/Marketplace/index.tsx
+++ src/layouts/Marketplace/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/legacy/image';
 
+import { useMediaQuery } from '@mui/material';
+
 import { Header } from 'components/Header';
 
 import { CenterLayout } from 'components/CenterLayout';
@@ -14,30 +16,51 @@ import { AccountCard } from 'components/AccountCard';
 
 import { Services } from './Services';
 import { Stores } from './Stores';
+import * as S from './styles';
 
 export function Marketplace() {
+  const isMobile = useMediaQuery('@media (max-width: 600px)');
+
   return (
     <>
       <Header>
         <ShowOffers key="showOffers" />
         <AccountCard />
       </Header>
-      <HeroLayout
-        imgSrc="/images/Casal_Quantum.png"
-        imgAlt="Two people smiling while working with a computer"
-        backgroundImageUrl="/images/Background_blue_01.svg"
-      >
-        <Image
-          src="/images/marketplace-hero-title.svg"
-          alt="Quantum marketplace logo"
-          width={380}
-          height={150}
-        />
-        <p>
-          Aproveite as melhores ofertas e estabelecimentos da cidade e receba
-          dinheiro de volta a cada compra para usar como quiser.
-        </p>
-      </HeroLayout>
+      {isMobile ? (
+        <S.HeroLayoutMobile>
+          <Image
+            src="/images/banner_marketplace_mobile_2.svg"
+            alt="Quantum marketplace logo"
+            width={396}
+            height={300}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            priority
+          />
+        </S.HeroLayoutMobile>
+      ) : (
+        <HeroLayout
+          imgSrc="/images/Casal_Quantum.png"
+          imgAlt="Two people smiling while working with a computer"
+          backgroundImageUrl="/images/Background_blue_01.svg"
+        >
+          <Image
+            src="/images/marketplace-hero-title.svg"
+            alt="Quantum marketplace logo"
+            width={380}
+            height={150}
+            priority
+          />
+          <p>
+            Aproveite as melhores ofertas e estabelecimentos da cidade e receba
+            dinheiro de volta a cada compra para usar como quiser.
+          </p>
+        </HeroLayout>
+      )}
       <CenterLayout>
         <Services />
         <Stores />
---
+++ src/layouts/Marketplace/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HeroLayoutMobile = styled.div`
+  width: 100%;
+  max-width: 100%;
+  max-height: 450px;
+  position: relative;
+`;
