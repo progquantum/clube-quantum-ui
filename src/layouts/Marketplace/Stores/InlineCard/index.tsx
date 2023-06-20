52d938488dbae517fd2a47ef8b6c6f2fa6119d83
Caio Castro <caio.castro@arloesitech.com.br>
2023-06-20 11:56:28 -0300

    feat: implement hook to get establihments and filter it using the name or category of establishment

--- src/layouts/Marketplace/Stores/InlineCard/index.tsx
+++ src/layouts/Marketplace/Stores/InlineCard/index.tsx
@@ -7,31 +7,32 @@ import { useTheme } from 'styled-components';
 import { ServiceCard } from 'layouts/Marketplace/Components/ServiceCard';
 
 import * as S from './styles';
+import { InlineCardProps } from './types';
 
-export function InlineCard() {
+export function InlineCard({ establishment }: InlineCardProps) {
   const { colors } = useTheme();
 
   return (
     <S.CardContainer>
       <ServiceCard height="90px">
         <Image
-          src="/images/dominos_logo.svg"
-          alt="TIM logo"
-          width={50.06}
-          height={48}
-          objectFit="contain"
+          src={establishment.logo_url}
+          alt={establishment.corporate_name}
+          title={establishment.corporate_name}
+          layout="fill"
+          style={{ borderRadius: '25px' }}
         />
       </ServiceCard>
 
       <S.Content>
-        <S.Title>Domino&apos;s Pizzaria</S.Title>
+        <S.Title>{establishment.corporate_name}</S.Title>
         <S.Rate>
           <RiStarFill size={13} color={colors.yellow} />
-          5,0
+          <span>0</span>
         </S.Rate>
-        <S.Distance>3,4 km</S.Distance>
-        <S.Discount>3%</S.Discount>
-        <S.Commerce>Restaurante</S.Commerce>
+        <S.Distance>0 km</S.Distance>
+        <S.Discount>{`${establishment.client_cashback}%`}</S.Discount>
+        <S.Commerce>{establishment.category_name}</S.Commerce>
       </S.Content>
     </S.CardContainer>
   );
