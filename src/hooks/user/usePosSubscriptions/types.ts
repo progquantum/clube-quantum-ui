18a878f4035a665330901a9201e7d8951d9b418f
juniorvilas <log_junior@hotmail.com>
2023-06-21 15:52:10 -0300

    refactor: change PosUser type

--- src/hooks/user/usePosSubscriptions/types.ts
+++ src/hooks/user/usePosSubscriptions/types.ts
@@ -5,15 +5,64 @@ export type Filter = {
 export type PosUser = {
   id: string;
   created_at: string;
-  legal_person: {
-    company_name: string;
-    cnpj: string;
-  };
-  individual_person: {
-    name: string;
-    cpf: string;
-  };
-  login: string;
+  document: string;
   phone: string;
+  login: string;
+  legal_person: LegalPerson;
+  individual_person: IndividualPerson;
+  establishment_pos: EstablishmentPos;
+  category: Category;
+  MarketplaceImages: MarketplaceImage[];
+  PosSerialNumber: PosSerialNumber[];
+  establishment_pos_working_hours: EstablishmentPosWorkingHour[];
 };
+export interface IndividualPerson {
+  name: string;
+  cpf: string;
+}
+
+export interface LegalPerson {
+  company_name: string;
+  cnpj: string;
+}
+export interface EstablishmentPos {
+  about: string;
+  id: string;
+  corporate_name: string;
+  lat_location: string;
+  long_location: string;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+}
+
+export interface MarketplaceImage {
+  id: string;
+  folder: string;
+  key: string;
+  url: string;
+}
+
+export interface PosSerialNumber {
+  id: string;
+  serial_number: string;
+}
+
+export interface EstablishmentPosWorkingHour {
+  id: string;
+  day_of_week: string;
+  opening_time: string;
+  closing_time: string;
+  cashback_split: CashbackSplit;
+}
+
+export interface CashbackSplit {
+  id: string;
+  client_cashback: string;
+  quantum_cashback: string;
+  total_cashback: string;
+}
+
 export type PosSubscriptionsProps = PosUser[];
