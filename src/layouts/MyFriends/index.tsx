e2d311d6b58790990cf665b9cf0d971d75994e82
Caio Castro <caio.castro@arloesitech.com.br>
2023-07-04 17:28:47 -0300

    refactor: add a loading component on My Friends page

--- src/layouts/MyFriends/index.tsx
+++ src/layouts/MyFriends/index.tsx
@@ -36,10 +36,6 @@ export function MyFriendsPage() {
   const hasPlan = user?.subscription?.is_active;
   const hasFriends = data?.friends.length > 0;
 
-  if (!hasFriends || isError || !hasPlan) {
-    return <NoFriends />;
-  }
-
   const months = {
     '01': 'Jan',
     '02': 'Fev',
@@ -54,6 +50,18 @@ export function MyFriendsPage() {
     '11': 'Nov',
     '12': 'Dez',
   };
+
+  if (isLoading && loading) {
+    return (
+      <DashboardLayout>
+        <Loader />
+      </DashboardLayout>
+    );
+  }
+
+  if (!hasFriends || isError || !hasPlan) {
+    return <NoFriends />;
+  }
 
   const formattedIndirectGains = indirectGains.map(item => {
     const m = item.date.split('-')[1];
@@ -77,132 +85,125 @@ export function MyFriendsPage() {
 
   return (
     <DashboardLayout>
-      {isLoading && loading ? (
-        <Loader />
-      ) : (
-        <S.CardsContainer>
-          <S.CardFriends>
-            <S.CardTitle>
-              <BsPersonBadge size={24} /> Amigos
-            </S.CardTitle>
-            {data?.friends.map(friend => (
-              <S.Friend key={friend.id}>
-                <S.TitleFriends>
-                  <S.Image
-                    src={
-                      friend.avatar_url?.length
-                        ? friend.avatar_url
-                        : getUserImagePlaceholder(friend.name)
-                    }
-                    alt={friend.name}
-                  />
-                  <div>
-                    <h5>{friend.name}</h5>
-                    {friend.inactivated_at !== null ? (
-                      <span>
-                        Inativo(a) desde {formatDate(friend.inactivated_at)}
-                      </span>
-                    ) : (
-                      <div>
-                        {friend.cashback !== 0 ? (
-                          <S.Cashback>
-                            {formatCashback(friend.cashback)}
-                          </S.Cashback>
-                        ) : (
-                          <S.NoCashback>R$ 0,00</S.NoCashback>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                </S.TitleFriends>
-                <S.AmountFriends>
-                  <BsFillPersonPlusFill size={16} />
-                  <p>{friend.amount_friends}</p>
-                </S.AmountFriends>
-              </S.Friend>
-            ))}
-            {totalPages > 1 && (
-              <S.PaginationContainer>
-                <ReactPaginate
-                  breakLabel="..."
-                  nextLabel={
-                    <IoIosArrowForward
-                      size={20}
-                      color={colors.mediumslateBlue}
-                    />
+      <S.CardsContainer>
+        <S.CardFriends>
+          <S.CardTitle>
+            <BsPersonBadge size={24} /> Amigos
+          </S.CardTitle>
+          {data?.friends.map(friend => (
+            <S.Friend key={friend.id}>
+              <S.TitleFriends>
+                <S.Image
+                  src={
+                    friend.avatar_url?.length
+                      ? friend.avatar_url
+                      : getUserImagePlaceholder(friend.name)
                   }
-                  onPageChange={onPageChange}
-                  pageCount={totalPages}
-                  previousLabel={
-                    <IoIosArrowBack size={20} color={colors.mediumslateBlue} />
-                  }
-                  containerClassName="paginationContainer"
-                  pageLinkClassName="pageLink"
-                  activeLinkClassName="activeLink"
+                  alt={friend.name}
                 />
-              </S.PaginationContainer>
-            )}
-          </S.CardFriends>
-
-          <S.FlexCards>
-            <S.IndirectFriends>
-              <S.CardTitle>
-                <BsPersonBadge size={24} /> Ganhos por indicações indiretas
-              </S.CardTitle>
-
-              <S.TotalFriends>
-                <p>Suas conexões indiretas</p>
-                <span>
-                  {indirectFriends?.indirect_friends_amount}/
-                  {indirectFriends?.indirect_friends_limit_amount}
-                </span>
-              </S.TotalFriends>
-
-              <progress
-                value={indirectFriends?.indirect_friends_amount}
-                max={indirectFriends?.indirect_friends_limit_amount}
-              />
-
-              <S.TotalFriends>
-                <p>
-                  Seu ganho com conexões <br /> indiretas esse mês foi de:
-                </p>
-                <span>
-                  {formatCashback(
-                    indirectFriends?.total_cashback_this_month_by_indirect_friends,
+                <div>
+                  <h5>{friend.name}</h5>
+                  {friend.inactivated_at !== null ? (
+                    <span>
+                      Inativo(a) desde {formatDate(friend.inactivated_at)}
+                    </span>
+                  ) : (
+                    <div>
+                      {friend.cashback !== 0 ? (
+                        <S.Cashback>
+                          {formatCashback(friend.cashback)}
+                        </S.Cashback>
+                      ) : (
+                        <S.NoCashback>R$ 0,00</S.NoCashback>
+                      )}
+                    </div>
                   )}
-                </span>
-              </S.TotalFriends>
-            </S.IndirectFriends>
+                </div>
+              </S.TitleFriends>
+              <S.AmountFriends>
+                <BsFillPersonPlusFill size={16} />
+                <p>{friend.amount_friends}</p>
+              </S.AmountFriends>
+            </S.Friend>
+          ))}
+          {totalPages > 1 && (
+            <S.PaginationContainer>
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel={
+                  <IoIosArrowForward size={20} color={colors.mediumslateBlue} />
+                }
+                onPageChange={onPageChange}
+                pageCount={totalPages}
+                previousLabel={
+                  <IoIosArrowBack size={20} color={colors.mediumslateBlue} />
+                }
+                containerClassName="paginationContainer"
+                pageLinkClassName="pageLink"
+                activeLinkClassName="activeLink"
+              />
+            </S.PaginationContainer>
+          )}
+        </S.CardFriends>
 
-            <S.IndirectFriends>
-              <S.CardTitle>
-                <BsPersonBadge size={24} /> Ganhos por indicações indiretas
-              </S.CardTitle>
-              <S.GraphicBar>
-                {graphInformation.map((item, index) => (
-                  <S.BarItem key={index} title={String(item.total)}>
-                    <S.Bar percentage={item.percentage} />
-                    <S.TitleBar>{item.month}</S.TitleBar>
-                  </S.BarItem>
-                ))}
-              </S.GraphicBar>
-            </S.IndirectFriends>
-            <S.InviteFriends>
-              <S.CardTitle>
-                <BsPersonBadge size={24} /> Convidar amigos
-              </S.CardTitle>
-              <S.InviteFriendsBody>
-                <h4>Não fique sozinho nessa!</h4>
-                <p>Convide seus amigos e ganhe cashback junto com eles!!!</p>
-                <Link href={INVITE_FRIENDS_PAGE}>
-                  <S.InviteFriendsButton>Convidar amigos</S.InviteFriendsButton>
-                </Link>
-              </S.InviteFriendsBody>
-            </S.InviteFriends>
-          </S.FlexCards>
-        </S.CardsContainer>
-      )}
+        <S.FlexCards>
+          <S.IndirectFriends>
+            <S.CardTitle>
+              <BsPersonBadge size={24} /> Ganhos por indicações indiretas
+            </S.CardTitle>
+
+            <S.TotalFriends>
+              <p>Suas conexões indiretas</p>
+              <span>
+                {indirectFriends?.indirect_friends_amount}/
+                {indirectFriends?.indirect_friends_limit_amount}
+              </span>
+            </S.TotalFriends>
+
+            <progress
+              value={indirectFriends?.indirect_friends_amount}
+              max={indirectFriends?.indirect_friends_limit_amount}
+            />
+
+            <S.TotalFriends>
+              <p>
+                Seu ganho com conexões <br /> indiretas esse mês foi de:
+              </p>
+              <span>
+                {formatCashback(
+                  indirectFriends?.total_cashback_this_month_by_indirect_friends,
+                )}
+              </span>
+            </S.TotalFriends>
+          </S.IndirectFriends>
+
+          <S.IndirectFriends>
+            <S.CardTitle>
+              <BsPersonBadge size={24} /> Ganhos por indicações indiretas
+            </S.CardTitle>
+            <S.GraphicBar>
+              {graphInformation.map((item, index) => (
+                <S.BarItem key={index} title={String(item.total)}>
+                  <S.Bar percentage={item.percentage} />
+                  <S.TitleBar>{item.month}</S.TitleBar>
+                </S.BarItem>
+              ))}
+            </S.GraphicBar>
+          </S.IndirectFriends>
+          <S.InviteFriends>
+            <S.CardTitle>
+              <BsPersonBadge size={24} /> Convidar amigos
+            </S.CardTitle>
+            <S.InviteFriendsBody>
+              <h4>Não fique sozinho nessa!</h4>
+              <p>Convide seus amigos e ganhe cashback junto com eles!!!</p>
+              <Link href={INVITE_FRIENDS_PAGE}>
+                <S.InviteFriendsButton>Convidar amigos</S.InviteFriendsButton>
+              </Link>
+            </S.InviteFriendsBody>
+          </S.InviteFriends>
+        </S.FlexCards>
+      </S.CardsContainer>
     </DashboardLayout>
   );
 }
