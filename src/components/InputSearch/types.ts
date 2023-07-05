3a7a132748e7a87b1c1b8c6776c6c29e4da9a84f
juniorvilas <log_junior@hotmail.com>
2023-07-05 12:47:14 -0300

    refactor: add name, onRequestClick and ...rest to inputSearch

--- src/components/InputSearch/types.ts
+++ src/components/InputSearch/types.ts
@@ -1,3 +1,7 @@
-export type InputSearchProps = {
+import { InputHTMLAttributes } from 'react';
+
+export type InputSearchProps = InputHTMLAttributes<HTMLInputElement> & {
+  name?: string;
   placeholder?: string;
+  onRequestClick?: () => void;
 };
