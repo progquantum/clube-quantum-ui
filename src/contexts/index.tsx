d9db36a000dbd7a80ed7dd0fbb93cf1ed7515309
Caio Castro <caio.castro@arloesitech.com.br>
2023-07-05 17:16:49 -0300

    refactor: add a global stale time of 20s and cache time of 10mins

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -26,6 +26,8 @@ export function AppProvider({
         defaultOptions: {
           queries: {
             refetchOnWindowFocus: false,
+            staleTime: 30000, // 30s
+            cacheTime: 10 * (60 * 1000), // 10 mins
           },
         },
       }),
