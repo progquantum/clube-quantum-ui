e767f7c693668f17f77b429a9009073913c7813e
Caio Castro <caio.castro@arloesitech.com.br>
2023-07-06 14:13:52 -0300

    feat: implement a simple unit test to PieChart

---
+++ src/layouts/DashboardAdm/ClientsByPlanPieChart/test.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import { ProviderMock } from '../../../../__test__/__mocks__/provider';
+import { ClientsByPlanPieChart } from '.';
+import { mockedClientsByPlan } from '../../../../__test__/__mocks__/mockedData/clientsByPlanPieChart';
+import { MockResizeObserver } from '../../../../__test__/__mocks__/resizeObserver';
+
+window.ResizeObserver = MockResizeObserver as typeof ResizeObserver;
+
+describe('Clients By Plan Pie Chart', () => {
+  it(' should render the PieChart component', () => {
+    const { getByText } = render(
+      <ProviderMock>
+        <ClientsByPlanPieChart clientsPerPlan={mockedClientsByPlan} />
+      </ProviderMock>,
+    );
+
+    const pieChart = getByText(/clientes por plano/i);
+    expect(pieChart).toBeInTheDocument();
+  });
+});
