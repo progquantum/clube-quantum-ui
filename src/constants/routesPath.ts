2f2d2eb87a5d297bdc0f5c228e6b394ab073cbdb
Caio Castro <caio.castro@arloesitech.com.br>
2023-07-21 17:09:52 -0300

    feat: add new page on sidebar and sidebar mobile

--- src/constants/routesPath.ts
+++ src/constants/routesPath.ts
@@ -39,3 +39,5 @@ export const PARTNER_REGISTRATION_PAGE = '/partner-registration';
 export const MANAGE_BANNER_PAGE = '/manage-banner';
 
 export const CANCELLATION_REQUEST_PAGE = '/cancellation-request';
+
+export const SMART_QUANTUM_REQUESTS_PAGE = '/smart-quantum-requests';
