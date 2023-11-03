ecea0cdf81c16a5190114eb0a4bc1ab4d5115010
Caio Castro <caio.castro@arloesitech.com.br>
2023-11-03 12:44:56 -0300

    refactor: move invite link container to a separate component

--- src/components/InviteFriends/index.tsx
+++ src/components/InviteFriends/index.tsx
@@ -1,34 +1,20 @@
-import { BsInstagram, BsPersonBadge } from 'react-icons/bs';
-
-import { FaFacebookF } from 'react-icons/fa';
-
-import { ImWhatsapp } from 'react-icons/im';
+import { BsPersonBadge } from 'react-icons/bs';
 
 import { useMe } from 'hooks/me/useMe';
 import { formatSepareteNumbers } from 'utils/formatters/formatSepareteNumbers';
-
-import { getCurrentNodeEnv } from 'utils/currentNodeEnv';
+import { useGetInviteLink } from 'hooks/me/useInviteLink';
+import { Loader } from 'components/Loader';
 
 import * as S from './styles';
+import { InviteLink } from './InviteLInk';
 
 export function InviteFriends() {
   const {
-    data: { invite_code, invites_accepted },
+    data: { invites_accepted },
   } = useMe();
+  const { data: invite, isLoading } = useGetInviteLink();
 
   const [totalFriends, limite] = formatSepareteNumbers(invites_accepted);
-
-  const inviteLink = `${getCurrentNodeEnv()}/signup?invite-code=${invite_code}`;
-
-  const handleCopyOnClick = async () => {
-    try {
-      await navigator.clipboard.writeText(inviteLink);
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        return err;
-      }
-    }
-  };
 
   const isLimitReached =
     totalFriends > 0 && limite > 0 && totalFriends === limite;
@@ -39,42 +25,15 @@ export function InviteFriends() {
         <BsPersonBadge />
         <S.TitleFriends>Convidar amigos</S.TitleFriends>
       </S.HeaderInviteFriends>
-      <S.TitleInviteFriends>Não fique sozinho nessa!</S.TitleInviteFriends>
-      <S.TextInviteFriends>
-        Convide seus amigos e ganhe cashback junto com eles!!!
-      </S.TextInviteFriends>
-      <S.InviteBox onClick={handleCopyOnClick}>
-        Seu link de convite é: <S.Bold>{inviteLink}</S.Bold>{' '}
-        <span>clique aqui para copiar!</span>
-      </S.InviteBox>
-      <S.SocialIconsBox>
-        <div>
-          <BsInstagram size={32} />
-        </div>
-        <div>
-          <FaFacebookF size={32} />
-        </div>
-        <div>
-          <ImWhatsapp size={32} />
-        </div>
-      </S.SocialIconsBox>
-      <S.InvitationsAcceptedBox>
-        <div>
-          <span>Convites aceitos</span>
-          <span>
-            {totalFriends ?? 0}/{limite}
-          </span>
-        </div>
-        <S.ProgressBar
-          quantityFilledInPercent={
-            totalFriends > limite
-              ? '100%'
-              : String((totalFriends / limite) * 100).concat('%')
-          }
-        >
-          <div />
-        </S.ProgressBar>
-      </S.InvitationsAcceptedBox>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <InviteLink
+          invite={invite}
+          limite={limite}
+          totalFriends={totalFriends}
+        />
+      )}
     </S.DivInviteFriends>
   );
 }
