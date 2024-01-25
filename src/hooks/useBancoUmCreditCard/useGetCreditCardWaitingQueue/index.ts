e58840b9ffe276aa4df89907bd5f6c517e107d47
Caio Castro <caio.castro@arloesitech.com.br>
2024-01-25 13:54:44 -0300

    feat: create hook to get list of users on credit card waiting queue

---
+++ src/hooks/useBancoUmCreditCard/useGetCreditCardWaitingQueue/index.ts
@@ -0,0 +1,52 @@
+import { AxiosError } from 'axios';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
+
+import { quantumClientQueue } from 'config/client';
+
+import {
+  GetCreditCardWaitingQueueParamValues,
+  GetCreditCardWaitingQueueResponseData,
+} from './types';
+
+const QUERY_KEY_GET_CREDIT_CARD_WAITING_QUEUE = 'get-credit-card-waiting-queue';
+
+export async function getCreditCardWaitingQueue(
+  paramValues: GetCreditCardWaitingQueueParamValues,
+) {
+  try {
+    const { data } =
+      await quantumClientQueue.get<GetCreditCardWaitingQueueResponseData>(
+        '/banco-um-credit-card/list',
+        { params: paramValues },
+      );
+    return data;
+  } catch (err: unknown) {
+    return Promise.reject(err);
+  }
+}
+
+export function useGetCreditCardWaitingQueue(
+  paramValues: Omit<
+    GetCreditCardWaitingQueueParamValues,
+    'page' | 'itemsPerPage'
+  >,
+) {
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading } = useQuery(
+    [paramValues, QUERY_KEY_GET_CREDIT_CARD_WAITING_QUEUE, page],
+    () =>
+      getCreditCardWaitingQueue({
+        ...paramValues,
+        page: String(page),
+        itemsPerPage: '1',
+      }),
+  );
+
+  const handlePageChange = (page: { selected: number }) => {
+    setPage(Math.min(page.selected + 1, data.info.totalPages));
+  };
+
+  return { data, handlePageChange, isLoading };
+}
