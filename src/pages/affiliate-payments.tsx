98f92d7ce823fa1affbcce52aa75811533b1bf5c
VitorLima1998 <vitor.xlr8@gmail.com>
2024-04-17 12:03:32 -0400

    feat: add affiliate payments page to mobile sidebar

---
+++ src/pages/affiliate-payments.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from 'layouts/DashboardLayout';
+
+import { AffliatePaymentLayout } from '../layouts/AffiliatePayment';
+
+export default function AffliatePayments() {
+  return (
+    <DashboardLayout>
+      <AffliatePaymentLayout />
+    </DashboardLayout>
+  );
+}
