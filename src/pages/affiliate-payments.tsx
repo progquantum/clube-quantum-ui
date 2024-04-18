6d98ac067cca22ee8e24f6b1c4cbe7f4489b18db
VitorLima1998 <vitor.xlr8@gmail.com>
2024-04-18 14:14:24 -0400

    refactor: add getServerSideProps function with authentication and role check

--- src/pages/affiliate-payments.tsx
+++ src/pages/affiliate-payments.tsx
@@ -1,6 +1,19 @@
+import { GetServerSideProps } from 'next';
+
 import { DashboardLayout } from 'layouts/DashboardLayout';
 
 import { AffliatePaymentLayout } from '../layouts/AffiliatePayment';
+import { withSSRAuth } from '../helpers/auth/withSSRAuth';
+import { roles } from '../constants/roles';
+
+export const getServerSideProps: GetServerSideProps = withSSRAuth(
+  async () => ({
+    props: {},
+  }),
+  {
+    roles: [roles.admin.id],
+  },
+);
 
 export default function AffliatePayments() {
   return (
