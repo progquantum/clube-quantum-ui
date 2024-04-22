08221ec218b6a805ba45dd42242cbcc6263d7280
VitorL98 <vitor.lima@arloesitech.com.br>
2024-04-22 15:29:52 -0400

    feat: refactor AffiliatePaymentLayout component to handle file upload and display payments data

--- src/layouts/AffiliatePayment/index.tsx
+++ src/layouts/AffiliatePayment/index.tsx
@@ -1,45 +1,26 @@
 import { IoIosArrowForward } from 'react-icons/io';
 import { useTheme } from 'styled-components';
 import { FiDownload } from 'react-icons/fi';
+import { useState, useRef } from 'react';
 
-import { useState } from 'react';
+import { useQueryClient } from 'react-query';
 
-import * as S from './styles';
+import toast from 'react-hot-toast';
 
 import { Modal } from '../../components/Modal';
 import { ModalContent } from './ModalContent';
+import { useUploadFileCsv } from '../../hooks/affiliate-payments/upload-file-csv';
 
-const data = [
-  {
-    file_name: 'arquivo1.txt',
-    file_string_date: '15/04/2024',
-    fake_url_download: 'http://exemplo.com/download/arquivo1.txt',
-  },
-  {
-    file_name: 'arquivo2.txt',
-    file_string_date: '15/04/2024',
-    fake_url_download: 'http://exemplo.com/download/arquivo2.txt',
-  },
-  {
-    file_name: 'arquivo3.txt',
-    file_string_date: '15/04/2024',
-    fake_url_download: 'http://exemplo.com/download/arquivo3.txt',
-  },
-  {
-    file_name: 'arquivo4.txt',
-    file_string_date: '15/04/2024',
-    fake_url_download: 'http://exemplo.com/download/arquivo4.txt',
-  },
-  {
-    file_name: 'arquivo5.txt',
-    file_string_date: '15/04/2024',
-    fake_url_download: 'http://exemplo.com/download/arquivo5.txt',
-  },
-];
+import { useListPayments } from '../../hooks/affiliate-payments/list-payments';
+
+import * as S from './styles';
 
 export function AffliatePaymentLayout() {
+  const queryClient = useQueryClient();
   const { gradients, colors } = useTheme();
+  const { mutateAsync } = useUploadFileCsv();
   const [modalOpen, setModalOpen] = useState(false);
+  const fileInputRef = useRef(null);
 
   const openModal = () => {
     setModalOpen(true);
@@ -48,6 +29,59 @@ export function AffliatePaymentLayout() {
   const closeModal = () => {
     setModalOpen(false);
   };
+
+  const handleFileUpload = async () => {
+    if (fileInputRef.current.files.length > 0) {
+      try {
+        const file = fileInputRef.current.files[0];
+        await mutateAsync(file);
+        toast.success('Arquivo carregado com sucesso');
+        queryClient.invalidateQueries('QUERY_KEY_UPLOAD_FILE_CSV');
+      } catch (error) {
+        toast.error('Erro ao carregar o arquivo');
+      }
+    }
+  };
+
+  const handleButtonClickUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  const {
+    data: paymentsData,
+    loading,
+    isError,
+  } = useListPayments({ page: 1, itemsPerPage: 8 });
+
+  let content;
+  if (loading) {
+    content = (
+      <S.TableRow>
+        <S.TableColumn>Loading...</S.TableColumn>
+      </S.TableRow>
+    );
+  } else if (isError) {
+    content = (
+      <S.TableRow>
+        <S.TableColumn>Erro ao carregar os dados</S.TableColumn>
+      </S.TableRow>
+    );
+  } else {
+    content = paymentsData.balancesFiles.map(payment => (
+      <S.TableRow key={payment.id}>
+        <S.TableColumn title={payment.key}>
+          {payment.key.substring(payment.key.indexOf('lote'))}
+        </S.TableColumn>
+
+        <S.TableColumnDate>
+          {new Date(payment.createAt).toLocaleDateString()}
+        </S.TableColumnDate>
+        <S.TableColumnAction onClick={() => window.open(payment.url, '_blank')}>
+          Baixar <FiDownload color={colors.royalblue} />
+        </S.TableColumnAction>
+      </S.TableRow>
+    ));
+  }
 
   return (
     <S.Container>
@@ -75,25 +109,20 @@ export function AffliatePaymentLayout() {
               <S.TableHeaderItem>Ação</S.TableHeaderItem>
             </S.TableRow>
           </S.TableHeaderContainer>
-          {data.map(item => (
-            <S.TableRow>
-              <S.TableColumn title={item.file_name}>
-                {item.file_name}
-              </S.TableColumn>
-              <S.TableColumnDate>{item.file_string_date}</S.TableColumnDate>
-              <S.TableColumnAction
-                onClick={() => console.log(item.fake_url_download)}
-              >
-                Baixar <FiDownload color={colors.royalblue} />
-              </S.TableColumnAction>
-            </S.TableRow>
-          ))}
+          {content}
         </S.Table>
       </S.TableContainer>
       <S.UploadButtonContainer>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileUpload}
+        />
         <S.UploadButton
           variant="secondary_outline"
           color={gradients.midnightBlueToMediumsLateBlue}
+          onClick={handleButtonClickUpload}
         >
           Carregar Arquivo
         </S.UploadButton>
