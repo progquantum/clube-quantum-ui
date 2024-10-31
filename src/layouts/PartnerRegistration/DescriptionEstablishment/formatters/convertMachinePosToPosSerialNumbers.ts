76f519c00791575b73392d560587ec1161f02a08
Gui-rosales <guilhermelexandre456@gmail.com>
2024-10-31 17:52:53 -0400

    refactor: update convertMachinePosToPosSerialNumbers to include secretToken and return structured object

--- src/layouts/PartnerRegistration/DescriptionEstablishment/formatters/convertMachinePosToPosSerialNumbers.ts
+++ src/layouts/PartnerRegistration/DescriptionEstablishment/formatters/convertMachinePosToPosSerialNumbers.ts
@@ -1,8 +1,16 @@
 interface MachinePos {
   id: string;
   serie: string;
+  secretToken: string;
 }
 
 export const convertMachinePosToPosSerialNumbers = (
   machinePos: MachinePos[],
-): string[] => machinePos.map(({ serie }) => serie);
+): {
+  pos_serial_number: string;
+  secretToken: string;
+}[] =>
+  machinePos.map(({ serie, secretToken }) => ({
+    pos_serial_number: serie,
+    secretToken,
+  }));
