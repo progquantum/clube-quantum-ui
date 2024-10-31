053789682f14665ac864a92d8143e796dd514a5f
Gui-rosales <guilhermelexandre456@gmail.com>
2024-10-31 17:51:33 -0400

    refactor: update pos_serial_numbers to pos_serials with new interface

--- src/hooks/establishment/useUpsertDescriptionCashback/types.ts
+++ src/hooks/establishment/useUpsertDescriptionCashback/types.ts
@@ -3,7 +3,8 @@ export type DescriptionCashback = {
   about: string;
   hours_opening: HoursOpening[];
   rules_cashback: RulesCashback[];
-  pos_serial_numbers: string[];
+  // pos_serial_numbers: string[];
+  pos_serials: PosSerials[];
   closed_days_of_week: string[];
 };
 
@@ -11,6 +12,11 @@ export interface HoursOpening {
   opening_time: string;
   closing_time: string;
   days_of_week: string[];
+}
+
+export interface PosSerials {
+  pos_serial_number: string;
+  secretToken: string;
 }
 
 export interface RulesCashback {
