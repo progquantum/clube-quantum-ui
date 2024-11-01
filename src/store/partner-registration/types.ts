afd20773d57009034c692d62edfedc596ecfe649
Gui-rosales <guilhermelexandre456@gmail.com>
2024-10-31 20:11:31 -0400

    refactor: update machinePos types

--- src/store/partner-registration/types.ts
+++ src/store/partner-registration/types.ts
@@ -17,6 +17,7 @@ export type CashBackRules = {
 export type MachinePos = {
   id: string;
   serie: string;
+  secret_token: string;
 }[];
 
 export type Logo = {
@@ -44,7 +45,7 @@ export type PartnerStore = {
   setRemoveCashBackRules: (id: string) => void;
   machinePos: MachinePos;
   resetMachinePos: () => void;
-  setMachinePos: (id: string, serie: string) => void;
+  setMachinePos: (id: string, serie: string, secret_token: string) => void;
   setRemoveMachinePos: (id: string) => void;
   aboutEstablishment: string;
   setAbout: (text: string) => void;
